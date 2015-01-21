e104f0c19e2b5933dd47227d68938211bc2c90af
Dmitry Yakimenko <detunized@gmail.com>
2015-01-22 00:43:39 +0100

    passwordbox: Compute password hash needed for login

--- .original/passwordbox/src/Fetcher.cs
+++ .original/passwordbox/src/Fetcher.cs
@@ -1,6 +1,8 @@
 // Copyright (C) 2015 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
+using System.Security.Cryptography;
+
 namespace PasswordBox
 {
     class Fetcher
@@ -14,6 +16,18 @@ namespace PasswordBox
         public static Session Login(string username, string password, IWebClient webClient)
         {
             return new Session("");
+        }
+
+        internal static string ComputePasswordHash(string username, string password)
+        {
+            var salt = Sha1Hex(username);
+            return Pbkdf2.GenerateSha256(password, salt, 10000, 256 / 8).ToHex();
+        }
+
+        internal static string Sha1Hex(string text)
+        {
+            using (var sha = new SHA1Managed())
+                return sha.ComputeHash(text.ToBytes()).ToHex();
         }
     }
 }
--- .original/passwordbox/test/FetcherTest.cs
+++ .original/passwordbox/test/FetcherTest.cs
@@ -21,5 +21,21 @@ namespace PasswordBox.Test
             Assert.NotNull(session);
             Assert.AreEqual("", session.Id);
         }
+
+        [Test]
+        public void ComputePasswordHash_returns_correct_result()
+        {
+            // Test data is generated with the PasswordBox JavaScript sources
+            var hash = Fetcher.ComputePasswordHash("username", "password");
+            Assert.AreEqual("bb5eeb368dd3d7ba5ab371c76ba5073e0a91f55697b81790bb34846d3e25f8e4", hash);
+        }
+
+        [Test]
+        public void HexSha1_returns_correct_result()
+        {
+            // Test data is from http://www.nsrl.nist.gov/testdata/
+            var hash = Fetcher.Sha1Hex("abc");
+            Assert.AreEqual("a9993e364706816aba3e25717850c26c9cd0d89d", hash);
+        }
     }
 }
