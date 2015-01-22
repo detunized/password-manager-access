e46f3d3969ec0b8c9cac48500264ee8e8ad8feae
Dmitry Yakimenko <detunized@gmail.com>
2015-01-22 22:46:08 +0100

    passwordbox: Example embryo

---
+++ .original/passwordbox/example/Program.cs
@@ -0,0 +1,12 @@
+// Copyright (C) 2015 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
