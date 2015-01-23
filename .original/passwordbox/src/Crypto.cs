835d05d9a16ba66b7e028d66d33aa8349375d3bd
Dmitry Yakimenko <detunized@gmail.com>
2015-01-23 17:55:30 +0100

    passwordbox: Decode and break up unput in Decrypt, handle errors

--- .original/passwordbox/src/Crypto.cs
+++ .original/passwordbox/src/Crypto.cs
@@ -1,16 +1,49 @@
 // Copyright (C) 2015 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
+using System;
+using System.Linq;
+
 namespace PasswordBox
 {
     static class Crypto
     {
         public static byte[] Decrypt(string encryptedBase64, string keyHex)
         {
-            if (encryptedBase64.Length == 0)
+            // Decode to binary
+            var encrypted = encryptedBase64.Decode64();
+            var key = keyHex.DecodeHex();
+
+            return Decrypt(encrypted, key);
+        }
+
+        public static byte[] Decrypt(byte[] encrypted, byte[] key)
+        {
+            if (encrypted.Length == 0)
                 return new byte[0];
 
-            return "".ToBytes();
+            if (encrypted.Length < 2)
+                throw new Exception("Cipher text is too short (version byte is missing)"); // TODO: Use custom exception!
+
+            // Version byte is at offset 1.
+            // We only support version 4 which seems to be the current.
+            var version = encrypted[1];
+            if (version != 4)
+                throw new Exception(string.Format("Unsupported cipher format version ({0})", version)); // TODO: Use custom exception!
+
+            if (encrypted.Length < 18)
+                throw new Exception("Cipher text is too short (IV is missing)"); // TODO: Use custom exception!
+
+            // Split encrypted into IV and cipher
+            var iv = encrypted.Skip(2).Take(16).ToArray();
+            var cipher = encrypted.Skip(18).ToArray();
+
+            return DecryptAes256Ccm(cipher, iv, key);
+        }
+
+        public static byte[] DecryptAes256Ccm(byte[] cipher, byte[] iv, byte[] key)
+        {
+            return new byte[0];
         }
     }
 }
--- .original/passwordbox/test/CryptoTest.cs
+++ .original/passwordbox/test/CryptoTest.cs
@@ -1,6 +1,7 @@
 // Copyright (C) 2015 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
+using System;
 using NUnit.Framework;
 
 namespace PasswordBox.Test
@@ -8,19 +9,53 @@ namespace PasswordBox.Test
     [TestFixture]
     class CryptoTest
     {
+        private const string KeyHex = "bc0d63541710541e493d1077e49e92523a4b7c53af1883266ed6c5be2f1b9562";
+        private static readonly byte[] Key = KeyHex.DecodeHex();
+
         [Test]
         public void Decrypt_returns_correct_result()
         {
             // TODO: Add real data
-            var decrypted = Crypto.Decrypt("", "");
-            Assert.AreEqual("", decrypted);
+            var decrypted = Crypto.Decrypt("", KeyHex);
+            Assert.IsEmpty(decrypted);
         }
 
         [Test]
-        public void Decrypt_returns_empty_on_empty_input()
+        public void Decrypt_returns_empty_on_empty_base64_input()
         {
-            var decrypted = Crypto.Decrypt("", "");
-            Assert.AreEqual("", decrypted);
+            var decrypted = Crypto.Decrypt("", KeyHex);
+            Assert.IsEmpty(decrypted);
+        }
+
+        [Test]
+        public void Decrypt_returns_empty_on_empty_binary_input()
+        {
+            var decrypted = Crypto.Decrypt("".ToBytes(), Key);
+            Assert.IsEmpty(decrypted);
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception),
+                           ExpectedMessage = "Cipher text is too short (version byte is missing)")]
+        public void Decrypt_throws_on_missing_format_byte()
+        {
+            Crypto.Decrypt("00".DecodeHex(), Key);
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception),
+                           ExpectedMessage = "Cipher text is too short (IV is missing)")]
+        public void Decrypt_throws_on_missing_iv()
+        {
+            Crypto.Decrypt("0004".DecodeHex(), Key);
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception),
+                           ExpectedMessage = "Unsupported cipher format version (5)")]
+        public void Decrypt_throws_on_unsupported_version()
+        {
+            Crypto.Decrypt("0005".DecodeHex(), Key);
         }
     }
 }
