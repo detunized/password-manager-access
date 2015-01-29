e632516c07a91fa50be317d649651208ae5a3a21
Dmitry Yakimenko <detunized@gmail.com>
2015-01-29 21:36:10 +0100

    passwordbox: Decrypt actual PB data

--- .original/passwordbox/src/Crypto.cs
+++ .original/passwordbox/src/Crypto.cs
@@ -8,22 +8,31 @@ namespace PasswordBox
 {
     static class Crypto
     {
-        public static byte[] Decrypt(string encryptedBase64, string keyHex)
+        // Decrypts a piece of data. Encrypted data is base64 encoded.
+        // The key is hex encoded. Only first 256 bits of key are used.
+        // Decrypted data is binary.
+        //
+        // Encrypted data is made up of the following parts:
+        // 1 byte at 0: ignored
+        // 1 byte at 1: format version (only 4 is supported)
+        // 16 bytes at 2: IV - initialized vector for AES-CCM encryption
+        // the rest at 18: ciphertext (encrypted data)
+        public static byte[] Decrypt(string keyHex, string encryptedBase64)
         {
             // Decode to binary
             var encrypted = encryptedBase64.Decode64();
             var key = keyHex.DecodeHex();
 
-            return Decrypt(encrypted, key);
+            return Decrypt(key, encrypted);
         }
 
-        public static byte[] Decrypt(byte[] encrypted, byte[] key)
+        public static byte[] Decrypt(byte[] key, byte[] encrypted)
         {
             if (encrypted.Length == 0)
                 return new byte[0];
 
             if (encrypted.Length < 2)
-                throw new Exception("Cipher text is too short (version byte is missing)"); // TODO: Use custom exception!
+                throw new Exception("Ciphertext is too short (version byte is missing)"); // TODO: Use custom exception!
 
             // Version byte is at offset 1.
             // We only support version 4 which seems to be the current.
@@ -32,18 +41,19 @@ namespace PasswordBox
                 throw new Exception(string.Format("Unsupported cipher format version ({0})", version)); // TODO: Use custom exception!
 
             if (encrypted.Length < 18)
-                throw new Exception("Cipher text is too short (IV is missing)"); // TODO: Use custom exception!
+                throw new Exception("Ciphertext is too short (IV is missing)"); // TODO: Use custom exception!
 
             // Split encrypted into IV and cipher
+            var ciphertext = encrypted.Skip(18).ToArray();
             var iv = encrypted.Skip(2).Take(16).ToArray();
-            var cipher = encrypted.Skip(18).ToArray();
 
-            return DecryptAes256Ccm(cipher, iv, key);
+            return DecryptAes256Ccm(key, ciphertext, iv);
         }
 
-        public static byte[] DecryptAes256Ccm(byte[] cipher, byte[] iv, byte[] key)
+        public static byte[] DecryptAes256Ccm(byte[] key, byte[] ciphertext, byte[] iv)
         {
-            return new byte[0];
+            var aes = new SjclAes(key);
+            return SjclCcm.Decrypt(aes, ciphertext, iv, new byte[0], 8);
         }
     }
 }
--- .original/passwordbox/test/CryptoTest.cs
+++ .original/passwordbox/test/CryptoTest.cs
@@ -10,44 +10,52 @@ namespace PasswordBox.Test
     class CryptoTest
     {
         private const string KeyHex = "bc0d63541710541e493d1077e49e92523a4b7c53af1883266ed6c5be2f1b9562";
+        private const string CiphertextBase64 = "AATXkbQnk41DJzqyfcFtcTaYE+ptuHwtC9TCmVdsK8/uXA==";
+        private static readonly byte[] Plaintext = "password".ToBytes();
         private static readonly byte[] Key = KeyHex.DecodeHex();
 
         [Test]
-        public void Decrypt_returns_correct_result()
+        public void Decrypt_strings_returns_correct_result()
         {
-            // TODO: Add real data
-            var decrypted = Crypto.Decrypt("", KeyHex);
-            Assert.IsEmpty(decrypted);
+            var decrypted = Crypto.Decrypt(KeyHex, CiphertextBase64);
+            Assert.AreEqual(Plaintext, decrypted);
+        }
+
+        [Test]
+        public void Decrypt_binary_returns_correct_result()
+        {
+            var decrypted = Crypto.Decrypt(Key, CiphertextBase64.Decode64());
+            Assert.AreEqual(Plaintext, decrypted);
         }
 
         [Test]
         public void Decrypt_returns_empty_on_empty_base64_input()
         {
-            var decrypted = Crypto.Decrypt("", KeyHex);
+            var decrypted = Crypto.Decrypt(KeyHex, "");
             Assert.IsEmpty(decrypted);
         }
 
         [Test]
         public void Decrypt_returns_empty_on_empty_binary_input()
         {
-            var decrypted = Crypto.Decrypt("".ToBytes(), Key);
+            var decrypted = Crypto.Decrypt(Key, "".ToBytes());
             Assert.IsEmpty(decrypted);
         }
 
         [Test]
         [ExpectedException(typeof(Exception),
-                           ExpectedMessage = "Cipher text is too short (version byte is missing)")]
+                           ExpectedMessage = "Ciphertext is too short (version byte is missing)")]
         public void Decrypt_throws_on_missing_format_byte()
         {
-            Crypto.Decrypt("00".DecodeHex(), Key);
+            Crypto.Decrypt(Key, "00".DecodeHex());
         }
 
         [Test]
         [ExpectedException(typeof(Exception),
-                           ExpectedMessage = "Cipher text is too short (IV is missing)")]
+                           ExpectedMessage = "Ciphertext is too short (IV is missing)")]
         public void Decrypt_throws_on_missing_iv()
         {
-            Crypto.Decrypt("0004".DecodeHex(), Key);
+            Crypto.Decrypt(Key, "0004".DecodeHex());
         }
 
         [Test]
@@ -55,7 +63,7 @@ namespace PasswordBox.Test
                            ExpectedMessage = "Unsupported cipher format version (5)")]
         public void Decrypt_throws_on_unsupported_version()
         {
-            Crypto.Decrypt("0005".DecodeHex(), Key);
+            Crypto.Decrypt(Key, "0005".DecodeHex());
         }
     }
 }
