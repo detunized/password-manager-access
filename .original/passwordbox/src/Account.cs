5e8dcecabae129cbbb06cdd409fcf861834b3236
Dmitry Yakimenko <detunized@gmail.com>
2015-01-30 02:06:23 +0100

    passwordbox: Account class

---
+++ .original/passwordbox/src/Account.cs
@@ -0,0 +1,23 @@
+// Copyright (C) 2015 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+namespace PasswordBox
+{
+    public class Account
+    {
+        public Account(string id, string name, string username, string password, string url)
+        {
+            Id = id;
+            Name = name;
+            Username = username;
+            Password = password;
+            Url = url;
+        }
+
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public string Url { get; private set; }
+    }
+}
---
+++ .original/passwordbox/test/AccountTest.cs
@@ -0,0 +1,28 @@
+// Copyright (C) 2015 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using NUnit.Framework;
+
+namespace PasswordBox.Test
+{
+    [TestFixture]
+    class AccountTest
+    {
+        [Test]
+        public void Account_properties_are_set()
+        {
+            var id = "1234567890";
+            var name = "name";
+            var username = "username";
+            var password = "password";
+            var url = "url";
+
+            var account = new Account(id, name, username, password, url);
+            Assert.AreEqual(id, account.Id);
+            Assert.AreEqual(name, account.Name);
+            Assert.AreEqual(username, account.Username);
+            Assert.AreEqual(password, account.Password);
+            Assert.AreEqual(url, account.Url);
+        }
+    }
+}
