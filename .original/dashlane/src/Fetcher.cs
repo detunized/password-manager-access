cce794e9189a584d8f651e82fbc7b1d76f2da4b7
Dmitry Yakimenko <detunized@gmail.com>
2016-02-03 00:37:32 +0100

    dashlane: TODO

--- .original/dashlane/src/Fetcher.cs
+++ .original/dashlane/src/Fetcher.cs
@@ -19,6 +19,8 @@ namespace Dashlane
 
         public static string Fetch(string username, string uki, IWebClient webClient)
         {
+            // TODO: Handle web exceptions!
+
             var response = webClient.UploadValues(LatestUrl, new NameValueCollection {
                 {"login", username},
                 {"lock", "nolock"},
