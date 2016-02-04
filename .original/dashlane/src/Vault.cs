6c3eb028485e6017f2396376f7e9a27ae21113ba
Dmitry Yakimenko <detunized@gmail.com>
2016-02-04 01:13:19 +0100

    dashlane: Open vault and parse full file

--- .original/dashlane/src/Vault.cs
+++ .original/dashlane/src/Vault.cs
@@ -1,6 +1,10 @@
 // Copyright (C) 2016 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
 namespace Dashlane
 {
     public class Vault
@@ -13,14 +17,20 @@ namespace Dashlane
 
         public static Vault Open(string username, string password, string uki, IWebClient webClient)
         {
-            return new Vault();
+            return new Vault(Fetcher.Fetch(username, uki, webClient), password);
         }
 
         public Account[] Accounts { get; private set; }
 
-        private Vault()
+        private Vault(JObject blob, string password)
         {
-            Accounts = new Account[] {};
+            var accounts = new List<Account>();
+
+            var fullFile = blob.GetString("fullBackupFile");
+            if (!string.IsNullOrWhiteSpace(fullFile))
+                accounts.AddRange(Parser.ExtractEncryptedAccounts(fullFile.Decode64(), password));
+
+            Accounts = accounts.OrderBy(i => i.Id).ToArray();
         }
     }
 }
--- .original/dashlane/test/VaultTest.cs
+++ .original/dashlane/test/VaultTest.cs
@@ -1,6 +1,9 @@
 // Copyright (C) 2016 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
+using System.Collections.Specialized;
+using System.IO;
+using Moq;
 using NUnit.Framework;
 
 namespace Dashlane.Test
@@ -8,5 +11,30 @@ namespace Dashlane.Test
     [TestFixture]
     class VaultTest
     {
+        public readonly string Username = "username";
+        public readonly string Password = "password";
+        public readonly string Uki = "uki";
+
+        [Test]
+        public void Open_opens_empty_vault()
+        {
+            Assert.That(
+                Vault.Open(Username, Password, Uki, SetupWebClient("{}")).Accounts,
+                Is.Empty);
+        }
+
+        //
+        // Helpers
+        //
+
+        private static IWebClient SetupWebClient(string response)
+        {
+            var webClient = new Mock<IWebClient>();
+            webClient
+                .Setup(x => x.UploadValues(It.IsAny<string>(), It.IsAny<NameValueCollection>()))
+                .Returns(response.ToBytes());
+
+            return webClient.Object;
+        }
     }
 }
