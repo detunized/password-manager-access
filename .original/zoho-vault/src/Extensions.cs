0301a04fc03d8ecc930737ef3f3f48a83ad4726a
Dmitry Yakimenko <detunized@gmail.com>
2016-09-14 12:31:32 +0200

    zoho-vault: Path access to JObject

--- .original/zoho-vault/src/Extensions.cs
+++ .original/zoho-vault/src/Extensions.cs
@@ -1,7 +1,9 @@
 // Copyright (C) 2016 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
+using System;
 using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace ZohoVault
 {
@@ -23,6 +25,45 @@ namespace ZohoVault
         public static string ToUtf8(this byte[] x)
         {
             return Encoding.UTF8.GetString(x);
+        }
+
+        //
+        // JObject
+        //
+
+        public static JToken At(this JObject j, string path)
+        {
+            var c = j.Root;
+            foreach (var i in path.Split('/'))
+            {
+                if (c.Type != JTokenType.Object)
+                    throw new ArgumentException("Must be nested objects all the way down");
+
+                c = c[i];
+                if (c == null)
+                    throw new ArgumentException("Path doesn't exist", path);
+
+            }
+
+            return c;
+        }
+
+        public static string StringAt(this JObject j, string path)
+        {
+            var s = j.At(path);
+            if (s.Type != JTokenType.String)
+                throw new ArgumentException("The value is not a string");
+
+            return (string)s;
+        }
+
+        public static int IntAt(this JObject j, string path)
+        {
+            var s = j.At(path);
+            if (s.Type != JTokenType.Integer)
+                throw new ArgumentException("The value is not an integer");
+
+            return (int)s;
         }
     }
 }
--- .original/zoho-vault/test/ExtensionsTest.cs
+++ .original/zoho-vault/test/ExtensionsTest.cs
@@ -1,6 +1,8 @@
 // Copyright (C) 2016 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
+using System;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 
 namespace ZohoVault.Test
@@ -26,6 +28,85 @@ namespace ZohoVault.Test
         {
             Assert.That(new byte[] {}.ToUtf8(), Is.EqualTo(""));
             Assert.That(TestBytes.ToUtf8(), Is.EqualTo(TestString));
+        }
+
+        [Test]
+        public void JObject_At_throws_on_invalid_path()
+        {
+            var j = JObject.Parse(@"{
+                'k1': 'v1',
+                'k2': {'k22': 'v22'},
+                'k3': {'k33': {'k333': 'v333'}}
+            }");
+            Assert.That(() => j.At("i1"), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => j.At("k1/k11"), Throws.TypeOf<ArgumentException>());
+
+            Assert.That(() => j.At("k2/i2"), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => j.At("k2/k22/i22"), Throws.TypeOf<ArgumentException>());
+
+            Assert.That(() => j.At("k3/i3"), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => j.At("k3/k33/i33"), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => j.At("k3/k33/k333/i333"), Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void JObject_At_throws_on_non_objects()
+        {
+            var j = JObject.Parse(@"{
+                'k1': [],
+                'k2': true,
+                'k3': 10
+            }");
+            Assert.That(() => j.At("k1/0"), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => j.At("k2/k22"), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => j.At("k3/k33/k333"), Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void JObject_StringAt_returns_string()
+        {
+            var j = JObject.Parse(@"{
+                'k1': 'v1',
+                'k2': {'k22': 'v22'},
+                'k3': {'k33': {'k333': 'v333'}}
+            }");
+            Assert.That(j.StringAt("k1"), Is.EqualTo("v1"));
+            Assert.That(j.StringAt("k2/k22"), Is.EqualTo("v22"));
+            Assert.That(j.StringAt("k3/k33/k333"), Is.EqualTo("v333"));
+        }
+
+        [Test]
+        public void JObject_StringAt_throws_on_non_stings()
+        {
+            var j = JObject.Parse(@"{
+                'k1': true,
+                'k2': 10,
+                'k3': 10.0,
+                'k4': [],
+                'k5': {},
+            }");
+            Assert.That(() => j.StringAt("k1"), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => j.StringAt("k2"), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => j.StringAt("k3"), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => j.StringAt("k4"), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => j.StringAt("k5"), Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void JObject_IntAt_throws_on_non_ints()
+        {
+            var j = JObject.Parse(@"{
+                'k1': true,
+                'k2': '10',
+                'k3': 10.0,
+                'k4': [],
+                'k5': {},
+            }");
+            Assert.That(() => j.IntAt("k1"), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => j.IntAt("k2"), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => j.IntAt("k3"), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => j.IntAt("k4"), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => j.IntAt("k5"), Throws.TypeOf<ArgumentException>());
         }
     }
 }
