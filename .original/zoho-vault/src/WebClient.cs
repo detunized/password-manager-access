9ed4df63d2bedb63f65d93a063c2288e3ebe74eb
Dmitry Yakimenko <detunized@gmail.com>
2016-09-14 09:29:50 +0200

    zoho-vault: WebClient

---
+++ .original/zoho-vault/src/WebClient.cs
@@ -0,0 +1,10 @@
+// Copyright (C) 2016 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+namespace ZohoVault
+{
+    [System.ComponentModel.DesignerCategory("Code")]
+    class WebClient: System.Net.WebClient, IWebClient
+    {
+    }
+}
