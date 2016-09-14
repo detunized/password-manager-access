d311f75c5359508ef68e33557efa66c44451ee91
Dmitry Yakimenko <detunized@gmail.com>
2016-09-14 10:39:05 +0200

    zoho-vault: Extend IWebClient with GET and header access

--- .original/zoho-vault/src/IWebClient.cs
+++ .original/zoho-vault/src/IWebClient.cs
@@ -2,11 +2,15 @@
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
 using System.Collections.Specialized;
+using System.Net;
 
 namespace ZohoVault
 {
     public interface IWebClient
     {
         byte[] UploadValues(string address, NameValueCollection data);
+        byte[] DownloadData(string address);
+        WebHeaderCollection Headers { get; }
+        WebHeaderCollection ResponseHeaders { get; }
     }
 }
