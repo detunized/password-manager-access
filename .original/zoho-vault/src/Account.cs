338f6672e0251620c868a244d0043db9bce4070c
Dmitry Yakimenko <detunized@gmail.com>
2016-09-20 18:33:14 +0200

    zoho-vault: Account (copied from dashlane-sharp)

---
+++ .original/zoho-vault/src/Account.cs
@@ -0,0 +1,25 @@
+// Copyright (C) 2016 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+namespace ZohoVault
+{
+    public class Account
+    {
+        public Account(string id, string name, string username, string password, string url, string note)
+        {
+            Id = id;
+            Name = name;
+            Username = username;
+            Password = password;
+            Url = url;
+            Note = note;
+        }
+
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public string Url { get; private set; }
+        public string Note { get; private set; }
+    }
+}
