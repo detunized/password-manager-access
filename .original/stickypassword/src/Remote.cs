b2396439a0fee3c45ec9469f38cd30aff6b742f8
Dmitry Yakimenko <detunized@gmail.com>
2017-01-07 03:39:58 +0100

    stickypassword: Use RestSharp instead of WebClient

--- .original/stickypassword/src/Remote.cs
+++ .original/stickypassword/src/Remote.cs
@@ -2,43 +2,57 @@
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
 using System;
-using System.Collections.Specialized;
-using System.Net;
-using System.Text;
+using System.Collections.Generic;
+using RestSharp;
 
 namespace StickyPassword
 {
     public static class Remote
     {
-        private const string ApiUrl = "https://spcb.stickypassword.com/SPCClient";
-        private const string TokenUrl = ApiUrl + "/GetCrpToken";
+        private const string ApiUrl = "https://spcb.stickypassword.com/SPCClient/";
 
-        public static string GetEncryptedToken(string username, string deviceId, IWebClient webClient)
+        public static string GetEncryptedToken(string username, string deviceId)
         {
-            SetRequestHeaders(webClient.Headers, deviceId, DateTime.Now);
-
-            // TODO: Handle network errors
-            var response = webClient.UploadValues(TokenUrl, new NameValueCollection
+            var response = Post("GetCrpToken", deviceId, DateTime.Now, new Dictionary<string, string>()
             {
                 {"uaid", username},
             });
 
-            // TODO: Parse JSON
-            return Encoding.UTF8.GetString(response);
+            // TODO: Parse the response
+            return response.Content;
         }
 
-        private static void SetRequestHeaders(WebHeaderCollection headers, string deviceId, DateTime timestamp)
+        private static string GetUserAgent(string deviceId)
         {
-            headers.Set("User-Agent",
-                string.Format("SP/8.0.3436 Prot=2 ID=#{0} Lng=EN Os=Android/4.4.4 Lic= LicStat= PackageID=", deviceId));
-            headers.Set("Date", timestamp.ToUniversalTime().ToString("r"));
-            headers.Set("Accept", "application/xml");
-            headers.Set("Pragma", "no-cache");
-            headers.Set("Cache-Control", "no-cache");
-            headers.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-            headers.Set("host", "spcb.stickypassword.com");
-            headers.Set("Connection", "Keep-Alive");
-            headers.Set("Accept-Encoding", "gzip");
+            return string.Format("SP/8.0.3436 Prot=2 ID={0} Lng=EN Os=Android/4.4.4 Lic= LicStat= PackageID=", deviceId);
+        }
+
+        private static RestClient CreateClient(string deviceId)
+        {
+            return new RestClient(ApiUrl)
+            {
+                UserAgent = GetUserAgent(deviceId)
+            };
+        }
+
+        private static IRestResponse Post(string endPoint, string deviceId, DateTime timestamp,
+            Dictionary<string, string> parameters)
+        {
+            var client = CreateClient(deviceId);
+
+            var request = new RestRequest(endPoint, Method.POST);
+            SetRequestHeaders(request, timestamp);
+
+            foreach (var i in parameters)
+                request.AddParameter(i.Key, i.Value);
+
+            return client.Execute(request);
+        }
+
+        private static void SetRequestHeaders(RestRequest request, DateTime timestamp)
+        {
+            request.AddHeader("Date", timestamp.ToUniversalTime().ToString("R"));
+            request.AddHeader("Accept", "application/xml");
         }
     }
 }
