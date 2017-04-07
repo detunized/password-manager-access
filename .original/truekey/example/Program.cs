873731768742e97d9368608cd0306d7ce456e298
Dmitry Yakimenko <detunized@gmail.com>
2017-04-07 10:28:53 +0200

    truekey: Actual POST JSON implementation via WebClient

--- .original/truekey/example/Program.cs
+++ .original/truekey/example/Program.cs
@@ -1,9 +1,12 @@
+using TrueKey;
+
 namespace Example
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var info = Remote.RegisetNewDevice("truekey-sharp");
         }
     }
 }
--- .original/truekey/src/HttpClient.cs
+++ .original/truekey/src/HttpClient.cs
@@ -2,6 +2,8 @@
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
 using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace TrueKey
 {
@@ -9,7 +11,13 @@ namespace TrueKey
     {
         public string Post(string url, Dictionary<string, string> parameters)
         {
-            return "";
+            using (var client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application/json; charset=UTF-8";
+
+                // TODO: Handle network errors
+                return client.UploadString(url, JsonConvert.SerializeObject(parameters));
+            }
         }
     }
 }
