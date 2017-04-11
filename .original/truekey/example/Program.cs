8a78008b54df78d180ca0d9917cda3bba77ae262
Dmitry Yakimenko <detunized@gmail.com>
2017-04-11 10:53:39 +0200

    truekey: Login flow so far

--- .original/truekey/example/Program.cs
+++ .original/truekey/example/Program.cs
@@ -6,7 +6,15 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            var info = Remote.RegisetNewDevice("truekey-sharp");
+            // Step 1: Register a new device and get a token and an id back.
+            var deviceInfo = Remote.RegisetNewDevice("truekey-sharp");
+
+            // Step 2: Parse the token to decode OTP information.
+            var otpInfo = Remote.ParseClientToken(deviceInfo.Token);
+
+            // Step 3: Validate the OTP info to make sure it's got only the
+            //         things we support at the moment.
+            Remote.ValidateOtpInfo(otpInfo);
         }
     }
 }
