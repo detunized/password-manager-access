d53fb6fc11a390816f76a040657c7ee8dda3eb86
Dmitry Yakimenko <detunized@gmail.com>
2017-04-25 10:14:41 +0200

    truekey: Doc comment for Gui

--- .original/truekey/src/Gui.cs
+++ .original/truekey/src/Gui.cs
@@ -3,6 +3,16 @@
 
 namespace TrueKey
 {
+    // This is the interface to be implemented by the user of the library.
+    // It's used to provide answers for the interactive two factor login
+    // process. The process is meant to interact with the user but it doesn't
+    // mean it has to. The implementation could simply return the first answer
+    // or something else like that. Have to be careful with the "wait" states
+    // though, as the answer should only come after the user is confirmed the
+    // second factor action, like clicked a button in the email or confirmed
+    // on the phone.
+    // The only reason this is a class and not an interface is because there's
+    // no way to have an enum inside an interface.
     public abstract class Gui
     {
         public enum Answer
