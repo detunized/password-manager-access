fe4c489e901ab79598c534b9b4b708a07aa94fd2
Dmitry Yakimenko <detunized@gmail.com>
2017-06-06 19:35:12 +0200

    1password: Example embryo

---
+++ .original/1password/example/Program.cs
@@ -0,0 +1,12 @@
+// Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
