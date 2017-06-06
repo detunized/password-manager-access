79379ddf45aa5c147777d4ab32a52bdc5fa15743
Dmitry Yakimenko <detunized@gmail.com>
2017-06-06 20:45:26 +0200

    1password: Start new session and all the things that come along

---
+++ .original/1password/src/Remote.cs
@@ -0,0 +1,85 @@
+// Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace OnePassword
+{
+    // TODO: Move out of here
+    public interface IHttpClient
+    {
+        string Get(string url, Dictionary<string, string> headers);
+    }
+
+    // TODO: Move out of here
+    public class HttpClient: IHttpClient
+    {
+        public string Get(string url, Dictionary<string, string> headers)
+        {
+            using (var client = new WebClient())
+            {
+                foreach (var i in headers)
+                    client.Headers[i.Key] = i.Value;
+
+                return client.DownloadString(url);
+            }
+        }
+    }
+
+    internal static class Remote
+    {
+        public class ClientInfo
+        {
+            public readonly string Username;
+            public readonly string Password;
+            public readonly string AccountKey;
+            public readonly string Uuid;
+
+            public ClientInfo(string username, string password, string accountKey, string uuid)
+            {
+                Username = username;
+                Password = password;
+                AccountKey = accountKey;
+                Uuid = uuid;
+            }
+        }
+
+        public class Session
+        {
+            public readonly string Id;
+
+            public Session(string id)
+            {
+                Id = id;
+            }
+        }
+
+        public static Session StartNewSession(ClientInfo clientInfo, IHttpClient http)
+        {
+            var response = Get(http, new[] {"auth", clientInfo.Username, clientInfo.Uuid, "-"});
+            var status = response["status"].ToString();
+            switch (status)
+            {
+            case "ok":
+                return new Session(response["sessionID"].ToString());
+            default:
+                // TODO: Use custom exception
+                throw new InvalidOperationException(
+                    string.Format(
+                        "Failed to start a new session, unsupported response status '{0}'",
+                        status));
+            }
+        }
+
+        internal static JObject Get(IHttpClient http, string[] urlComponents)
+        {
+            // TODO: Handle network errors
+            var response = http.Get(string.Join("/", urlComponents),
+                                    new Dictionary<string, string>());
+            return JObject.Parse(response);
+        }
+    }
+}
---
+++ .original/1password/test/RemoteTest.cs
@@ -0,0 +1,63 @@
+// Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using System.Collections.Generic;
+using System.IO;
+using Moq;
+using NUnit.Framework;
+
+namespace OnePassword.Test
+{
+    [TestFixture]
+    class RemoteTest
+    {
+        [Test]
+        public void StartNewSession_returns_session()
+        {
+            var client = SetupGetWithFixture("start-new-session-response");
+            var session = Remote.StartNewSession(ClientInfo, client.Object);
+
+            Assert.That(session.Id, Is.EqualTo(SessionId));
+        }
+
+        //
+        // Data
+        //
+
+        private const string Username = "username";
+        private const string Password = "password";
+        private const string AccountKey = "account-key";
+        private const string Uuid = "uuid";
+
+        private const string SessionId = "TOZVTFIFBZGFDFNE5KSZFY7EZY";
+
+        private static readonly Remote.ClientInfo ClientInfo =
+            new Remote.ClientInfo(username: Username,
+                                  password: Password,
+                                  accountKey: AccountKey,
+                                  uuid: Uuid);
+
+        //
+        // Helpers
+        //
+
+        private static Mock<IHttpClient> SetupGetWithFixture(string name)
+        {
+            return SetupGet(ReadFixture(name));
+        }
+
+        private static Mock<IHttpClient> SetupGet(string response)
+        {
+            var mock = new Mock<IHttpClient>();
+            mock.Setup(x => x.Get(It.IsAny<string>(),
+                                  It.IsAny<Dictionary<string, string>>()))
+                .Returns(response);
+            return mock;
+        }
+
+        private static string ReadFixture(string name)
+        {
+            return File.ReadAllText(string.Format("Fixtures/{0}.json", name));
+        }
+    }
+}
