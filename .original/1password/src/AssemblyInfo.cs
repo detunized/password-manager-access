991cb99c198ae022d887f83bd31b9df93ee313a9
Dmitry Yakimenko <detunized@gmail.com>
2017-06-06 20:42:03 +0200

    1password: Make internals of the main lib visible to the test lib

--- .original/1password/src/AssemblyInfo.cs
+++ .original/1password/src/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -36,3 +37,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("OnePassword.Test")]
