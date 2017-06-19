109dff1299407fdd977b18320c0d0f4a10b64413
Dmitry Yakimenko <detunized@gmail.com>
2017-06-19 10:32:11 +0200

    1password: AccountKey class

---
+++ .original/1password/src/AccountKey.cs
@@ -0,0 +1,69 @@
+// Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using System;
+
+namespace OnePassword
+{
+    internal class AccountKey
+    {
+        public readonly string Format;
+        public readonly string Uuid;
+        public readonly string Key;
+
+        public static AccountKey Parse(string str)
+        {
+            var s = str.ToUpperInvariant().Replace("-", "");
+            if (s.Length < 2)
+                throw new InvalidOperationException("Invalid account key: too short");
+
+            var format = s.Substring(0, 2);
+            switch (format)
+            {
+            case "A2":
+                if (s.Length != 33)
+                    throw new InvalidOperationException(
+                        "Invalid account key: incorrect length for 'A2' format");
+                break;
+            case "A3":
+                if (s.Length != 34)
+                    throw new InvalidOperationException(
+                        "Invalid account key: incorrect length for 'A3' format");
+                break;
+            default:
+                throw new InvalidOperationException(
+                    string.Format("Invalid account key: unknown format '{0}'", format));
+            }
+
+            return new AccountKey(format: format,
+                                  uuid: s.Substring(2, 6),
+                                  key: s.Substring(8));
+        }
+
+        public AccountKey(string format, string uuid, string key)
+        {
+            Format = format;
+            Uuid = uuid;
+            Key = key;
+        }
+
+        public byte[] Hash()
+        {
+            return Crypto.Hkdf(method: Format,
+                               ikm: Key.ToBytes(),
+                               salt: Uuid.ToBytes());
+        }
+
+        public byte[] CombineWith(byte[] bytes)
+        {
+            var h = Hash();
+            if (h.Length != bytes.Length)
+                throw new InvalidOperationException("Size doesn't match hash function");
+
+            for (int i = 0; i < h.Length; ++i)
+                h[i] ^= bytes[i];
+
+            return h;
+        }
+    }
+}
---
+++ .original/1password/test/AccountKeyTest.cs
@@ -0,0 +1,93 @@
+// Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using System;
+using NUnit.Framework;
+
+namespace OnePassword.Test
+{
+    [TestFixture]
+    public class AccountKeyTest
+    {
+        [Test]
+        public void Parse_returns_parsed_format_A3_key()
+        {
+            var key = AccountKey.Parse(KeyString);
+
+            Assert.That(key.Format, Is.EqualTo("A3"));
+            Assert.That(key.Uuid, Is.EqualTo("RTN9SA"));
+            Assert.That(key.Key, Is.EqualTo("DY9445Y5FF96X6E7B5GPFA95R9"));
+        }
+
+        [Test]
+        public void Parse_returns_parsed_format_A2_key()
+        {
+            // This a made up test. I don't have an existing example of a key in this format.
+            var key = AccountKey.Parse("A2-RTN9SA-DY9445Y5FF96X6E7B5GPFA95R");
+
+            Assert.That(key.Format, Is.EqualTo("A2"));
+            Assert.That(key.Uuid, Is.EqualTo("RTN9SA"));
+            Assert.That(key.Key, Is.EqualTo("DY9445Y5FF96X6E7B5GPFA95R"));
+        }
+
+        [Test]
+        public void Parse_throws_on_invalid_key_format()
+        {
+            var keys = new[]
+            {
+                "", // Too short for format
+                "A", // Too short for format
+                "A2", // Too short
+                "A3", // Too short
+                "A2-RTN9SA-DY9445Y5FF96X6E7B5GPFA95", // Too short for A2
+                "A2-RTN9SA-DY9445Y5FF96X6E7B5GPFA95R9", // Too long for A2
+                "A3-RTN9SA-DY9445Y5FF96X6E7B5GPFA95R", // Too short for A3
+                "A3-RTN9SA-DY9445Y5FF96X6E7B5GPFA95R99", // Too long for A3
+                "A3-RTN9SA-DY9445Y-FF96X6E7B-GPFA95R9" // Too short for A3 after removing of "-"
+            };
+
+            foreach (var key in keys)
+                Assert.That(() => AccountKey.Parse(key), Throws.TypeOf<InvalidOperationException>());
+        }
+
+        [Test]
+        public void Hash_returnes_hashed_key()
+        {
+            Assert.That(Key.Hash(),
+                        Is.EqualTo("ZlI2kRote1dv7uflTenyIp5jBE0u-7Fl4aIiE0D9L-g".Decode64()));
+        }
+
+        [Test]
+        public void CombineWith_returnes_hashed_key()
+        {
+            Assert.That(Key.CombineWith("All your base are belong to us!!".ToBytes()),
+                        Is.EqualTo("Jz5asWNCDiVPjIaWKMmTUPtDZihClN8CwdZNMzWODsk".Decode64()));
+        }
+
+        [Test]
+        public void CombineWith_throws_on_incorrect_length()
+        {
+            var bytes = new[]
+            {
+                "",
+                "A",
+                "All your base are belong to us",
+                "All your base are belong to us!",
+                "All your base are belong to us!!!",
+            };
+
+            foreach (var b in bytes)
+                Assert.That(() => Key.CombineWith(b.ToBytes()),
+                            Throws.TypeOf<InvalidOperationException>());
+        }
+
+        //
+        // Data
+        //
+
+        private const string KeyString = "A3-RTN9SA-DY9445Y5FF96X6E7B5GPFA95R9";
+        private static readonly AccountKey Key = new AccountKey("A3",
+                                                                "RTN9SA",
+                                                                "DY9445Y5FF96X6E7B5GPFA95R9");
+    }
+}
