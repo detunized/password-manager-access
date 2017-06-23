7c11108d93a5e99b0b91e54d53f208683fb2f8e5
Dmitry Yakimenko <detunized@gmail.com>
2017-06-23 13:40:50 +0200

    1password: Test GHash separately

--- .original/1password/test/AesGcmTest.cs
+++ .original/1password/test/AesGcmTest.cs
@@ -18,6 +18,20 @@ namespace OnePassword.Test
             }
         }
 
+        [Test]
+        public void GHash()
+        {
+            foreach (var i in TestCases)
+            {
+                var hash = AesGcm.GHash(i.HashKey,
+                                        i.AuthData,
+                                        i.AuthData.Length,
+                                        i.Ciphertext,
+                                        i.Ciphertext.Length);
+                Assert.That(hash, Is.EqualTo(i.GHash));
+            }
+        }
+
         //
         // Data
         //
@@ -28,15 +42,30 @@ namespace OnePassword.Test
             public readonly byte[] Plaintext;
             public readonly byte[] Iv;
             public readonly byte[] AuthData;
+            public readonly byte[] Ciphertext;
             public readonly byte[] Expected;
 
-            public TestCase(string key, string plaintext, string iv, string authData, string ciphertext, string tag)
+            public readonly byte[] HashKey;
+            public readonly byte[] GHash;
+
+            public TestCase(string key,
+                            string plaintext,
+                            string iv,
+                            string authData,
+                            string ciphertext,
+                            string tag,
+                            string hashKey,
+                            string gHash)
             {
                 Key = key.DecodeHex();
                 Plaintext = plaintext.DecodeHex();
                 Iv = iv.DecodeHex();
                 AuthData = authData.DecodeHex();
+                Ciphertext = ciphertext.DecodeHex();
                 Expected = (ciphertext + tag).DecodeHex();
+
+                HashKey = hashKey.DecodeHex();
+                GHash = gHash.DecodeHex();
             }
         }
 
@@ -49,7 +78,9 @@ namespace OnePassword.Test
                 iv: "12153524c0895e81b2c28465",
                 authData: "d609b1f056637a0d46df998d88e5222ab2c2846512153524c0895e8108000f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f30313233340001",
                 ciphertext: "",
-                tag: "2f0bc5af409e06d609ea8b7d0fa5ea50"),
+                tag: "2f0bc5af409e06d609ea8b7d0fa5ea50",
+                hashKey: "286d73994ea0ba3cfd1f52bf06a8acf2",
+                gHash: "5e4691528f50e5ab5ec346a7bc264a46"),
         };
     }
 }
