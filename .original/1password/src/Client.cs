b92e3e6e72442bd98c891e9259007df9685a5fda
Dmitry Yakimenko <detunized@gmail.com>
2017-06-29 10:58:11 +0200

    1password: Verify the session key with the server

--- .original/1password/src/Client.cs
+++ .original/1password/src/Client.cs
@@ -2,6 +2,9 @@
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
 using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace OnePassword
 {
@@ -9,9 +12,8 @@ namespace OnePassword
     {
         public const string ApiUrl = "https://my.1password.com/api/v1";
 
-        public Client(IHttpClient http)
+        public Client(IHttpClient http): this(new JsonHttpClient(http, ApiUrl))
         {
-            _http = new JsonHttpClient(http, ApiUrl);
         }
 
         public Vault OpenVault(ClientInfo clientInfo)
@@ -22,12 +24,20 @@ namespace OnePassword
             // Step 2: Perform SRP exchange
             var sessionKey = Srp.Perform(clientInfo, session, _http);
 
+            // Step 3: Verify the key with the server
+            VerifySessionKey(session, sessionKey);
+
             return new Vault();
         }
 
         //
         // Internal
         //
+
+        internal Client(JsonHttpClient http)
+        {
+            _http = http;
+        }
 
         internal Session StartNewSession(ClientInfo clientInfo)
         {
@@ -45,6 +55,44 @@ namespace OnePassword
                         "Failed to start a new session, unsupported response status '{0}'",
                         status));
             }
+        }
+
+        internal void VerifySessionKey(Session session, AesKey sessionKey)
+        {
+            var response = PostJson("auth/verify", new {sessionID = session.Id}, sessionKey);
+
+            // Just to verify that it's a valid JSON and it has some keys.
+            // Technically it should have failed by now either in decrypt or JSON parse
+            response.StringAt("userUuid");
+        }
+
+        internal JObject PostJson(string endpoint, object parameters, AesKey sessionKey)
+        {
+            var payload = JsonConvert.SerializeObject(parameters);
+            var encryptedPayload = sessionKey.Encrypt(payload.ToBytes());
+
+            var response = _http.Post(endpoint, new Dictionary<string, object>
+            {
+                {"kid", encryptedPayload.KeyId},
+                {"enc", encryptedPayload.Scheme},
+                {"cty", encryptedPayload.Container},
+                {"iv", encryptedPayload.Iv.ToBase64()},
+                {"data", encryptedPayload.Ciphertext.ToBase64()},
+            });
+
+            return DecryptAndParse(response, sessionKey);
+        }
+
+        internal JObject DecryptAndParse(JObject response, AesKey sessionKey)
+        {
+            var encrypted = new Encrypted(keyId: response.StringAt("kid"),
+                                          scheme: response.StringAt("enc"),
+                                          container: response.StringAt("cty"),
+                                          iv: response.StringAt("iv").Decode64(),
+                                          ciphertext: response.StringAt("data").Decode64());
+            var decrypted = sessionKey.Decrypt(encrypted);
+
+            return JObject.Parse(decrypted.ToUtf8());
         }
 
         //
---
+++ .original/1password/test/ClientTest.cs
@@ -0,0 +1,22 @@
+// Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using NUnit.Framework;
+
+namespace OnePassword.Test
+{
+    [TestFixture]
+    public class ClientTest
+    {
+        [Test]
+        public void VerifySessionKey_works()
+        {
+            var sessionKey = new AesKey(
+                TestData.SessionId,
+                "1c45a129b9e96b2f2eae330e8fd3c2dbb9dbe71b696d19823f3fa031b3218aad".DecodeHex());
+
+            var http = JsonHttpClientTest.SetupPostWithFixture("verify-key-response");
+            new Client(http.Object).VerifySessionKey(TestData.Session, sessionKey);
+        }
+    }
+}
