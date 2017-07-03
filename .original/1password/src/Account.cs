abfe58206bc798001ef03a083f7aaa5cfff33206
Dmitry Yakimenko <detunized@gmail.com>
2017-07-03 10:55:37 +0200

    1password: Account class

---
+++ .original/1password/src/Account.cs
@@ -0,0 +1,25 @@
+// Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+namespace OnePassword
+{
+    public class Account
+    {
+        public readonly int Id;
+        public readonly string Name;
+        public readonly string Username;
+        public readonly string Password;
+        public readonly string Url;
+        public readonly string Note;
+
+        public Account(int id, string name, string username, string password, string url, string note)
+        {
+            Id = id;
+            Name = name;
+            Username = username;
+            Password = password;
+            Url = url;
+            Note = note;
+        }
+    }
+}
---
+++ .original/1password/test/AccountTest.cs
@@ -0,0 +1,30 @@
+// Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using NUnit.Framework;
+
+namespace OnePassword.Test
+{
+    [TestFixture]
+    class AccountTest
+    {
+        [Test]
+        public void Account_properties_are_set()
+        {
+            var id = 1337;
+            var name = "name";
+            var username = "username";
+            var password = "password";
+            var url = "url";
+            var note = "note";
+            var account = new Account(id, name, username, password, url, note);
+
+            Assert.That(account.Id, Is.EqualTo(id));
+            Assert.That(account.Name, Is.EqualTo(name));
+            Assert.That(account.Username, Is.EqualTo(username));
+            Assert.That(account.Password, Is.EqualTo(password));
+            Assert.That(account.Url, Is.EqualTo(url));
+            Assert.That(account.Note, Is.EqualTo(note));
+        }
+    }
+}
