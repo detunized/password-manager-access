63e0c6dc05c688a352ce74ddbfd417ed2c6472a6
Dmitry Yakimenko <detunized@gmail.com>
2017-07-05 00:08:35 +0200

    1password: Enable TLS 1.1 and 1.2

--- .original/1password/src/HttpClient.cs
+++ .original/1password/src/HttpClient.cs
@@ -8,6 +8,23 @@ namespace OnePassword
 {
     public class HttpClient: IHttpClient
     {
+        static HttpClient()
+        {
+            // This enables TLS 1.2 amongst other things. Without this .NET 4.0 or 4.5 fails to
+            // connect to 1password.com. This should be not needed with .NET 4.6. For TLS 1.1 and 1.2
+            // we have to use the numerical values for .NET 4.0. Strangely they work, thought
+            // actual constants are only defined in .NET 4.5.
+            // see https://stackoverflow.com/a/40295737/362938
+            // see http://blogs.perficient.com/microsoft/2016/04/tsl-1-2-and-net-support/
+            //
+            // Warning: This is global (or app domain level) so it could mess up some things for
+            //          the application that uses the library!
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 |
+                                                   SecurityProtocolType.Tls |
+                                                   (SecurityProtocolType)768 | // Tls11, enum is defined in 4.5
+                                                   (SecurityProtocolType)3072; // Tls12, enum is defined in 4.5
+        }
+
         public string Get(string url, Dictionary<string, string> headers)
         {
             using (var client = new WebClient())
