0a0fd9b104b819092489204d2f83a5ae0ddba35f
Dmitry Yakimenko <detunized@gmail.com>
2017-09-17 19:42:10 +0200

    1password: Removed unused using

--- .original/1password/test/ClientTest.cs
+++ .original/1password/test/ClientTest.cs
@@ -1,7 +1,6 @@
 // Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
-using System;
 using Moq;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
--- .original/1password/test/SrpTest.cs
+++ .original/1password/test/SrpTest.cs
@@ -1,7 +1,6 @@
 // Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
-using System;
 using System.Numerics;
 using NUnit.Framework;
 
