4908f9ec4a29b7ff23aeeecf407395385d490104
Dmitry Yakimenko <detunized@gmail.com>
2017-11-06 19:02:26 +0100

    roboform: string.Decode64

--- .original/roboform/src/Extensions.cs
+++ .original/roboform/src/Extensions.cs
@@ -17,6 +17,11 @@ namespace RoboForm
             return Encoding.UTF8.GetBytes(s);
         }
 
+        public static byte[] Decode64(this string s)
+        {
+            return Convert.FromBase64String(s);
+        }
+
         public static string ToBase64(this string s)
         {
             return s.ToBytes().ToBase64();
