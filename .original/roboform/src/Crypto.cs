d34021850b67bf1192dcdd92004866d166df02da
Dmitry Yakimenko <detunized@gmail.com>
2017-11-07 11:53:30 +0100

    roboform: Hash password

---
+++ .original/roboform/src/Crypto.cs
@@ -0,0 +1,25 @@
+// Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using System.Security.Cryptography;
+
+namespace RoboForm
+{
+    internal static class Crypto
+    {
+        public static byte[] HashPassword(string password, Client.AuthInfo authInfo)
+        {
+            var passwordBytes = password.ToBytes();
+            if (authInfo.IsMd5)
+                passwordBytes = Md5(passwordBytes);
+
+            return Pbkdf2.Generate(passwordBytes, authInfo.Salt, authInfo.IterationCount, 32);
+        }
+
+        public static byte[] Md5(byte[] data)
+        {
+            using (var md5 = MD5.Create())
+                return md5.ComputeHash(data);
+        }
+    }
+}
---
+++ .original/roboform/test/CryptoTest.cs
@@ -0,0 +1,44 @@
+// Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using NUnit.Framework;
+
+namespace RoboForm.Test
+{
+    [TestFixture]
+    class CryptoTest
+    {
+        [Test]
+        public void HashPassword_returns_hashed_password()
+        {
+            // Generated with the original JavaScript code
+            Assert.That(
+                Crypto.HashPassword(Password, AuthInfo),
+                Is.EqualTo("b+rd7TUt65+hdE7+lHCBPPWHjxbq6qs0y7zufYfqHto=".Decode64()));
+        }
+
+        [Test]
+        public void Md5_returns_hashed_message()
+        {
+            // From https://www.nist.gov/itl/ssd/software-quality-group/nsrl-test-data
+            Assert.That(
+                Crypto.Md5("abc".ToBytes()),
+                Is.EqualTo("kAFQmDzST7DWlj99KOF/cg==".Decode64()));
+        }
+
+        //
+        // Data
+        //
+
+        private const string Password = "h74@aB$SCt9dTBQ3%rmAVN3oOmtGLt58Nix7!3z%vUO4Ni07rfjutHRbhJ9!SkOk";
+
+        // TODO: Generate a test case with MD5
+        private static readonly Client.AuthInfo AuthInfo =
+            new Client.AuthInfo(sid: "",
+                                data: "",
+                                nonce: "",
+                                salt: "A+Ft8UM674OZOOjUjXCdbw==".Decode64(),
+                                iterationCount: 4096,
+                                isMd5: false);
+    }
+}
