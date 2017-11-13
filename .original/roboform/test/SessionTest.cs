6109c5722847c38713d411066f675cca61ecec9f
Dmitry Yakimenko <detunized@gmail.com>
2017-11-13 23:59:21 +0100

    roboform: Session tests

---
+++ .original/roboform/test/SessionTest.cs
@@ -0,0 +1,29 @@
+// Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using NUnit.Framework;
+
+namespace RoboForm.Test
+{
+    [TestFixture]
+    class SessionTest
+    {
+        [Test]
+        public void Token_is_set()
+        {
+            Assert.That(new Session("token", "").Token, Is.EqualTo("token"));
+        }
+
+        [Test]
+        public void DeviceId_is_set()
+        {
+            Assert.That(new Session("", "device-id").DeviceId, Is.EqualTo("device-id"));
+        }
+
+        [Test]
+        public void Header_is_set()
+        {
+            Assert.That(new Session("token", "device-id").Header, Is.EqualTo("token; device-id"));
+        }
+    }
+}
