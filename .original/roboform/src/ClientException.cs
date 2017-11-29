e787d116f1c5a824c924dbe88738ae36bdefcd57
Dmitry Yakimenko <detunized@gmail.com>
2017-11-29 18:36:37 +0100

    roboform: Custom exceptions

---
+++ .original/roboform/src/ClientException.cs
@@ -0,0 +1,35 @@
+// Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using System;
+
+namespace RoboForm
+{
+    public class ClientException: BaseException
+    {
+        public enum FailureReason
+        {
+            IncorrectCredentials,
+            NetworkError,
+            InvalidResponse,
+            RespondedWithError,
+            ParseError,
+            UnsupportedFeature,
+            InvalidOperation,
+            UnknownError
+        }
+
+        public ClientException(FailureReason reason, string message):
+            this(reason, message, null)
+        {
+        }
+
+        public ClientException(FailureReason reason, string message, Exception innerException):
+            base(message, innerException)
+        {
+            Reason = reason;
+        }
+
+        public FailureReason Reason { get; private set; }
+    }
+}
