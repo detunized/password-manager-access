95cdb352722a72dc33f6e2589653741c325a05b5
Dmitry Yakimenko <detunized@gmail.com>
2018-01-22 11:47:19 +0100

    roboform: Public function to generate a random device id

--- .original/roboform/src/Vault.cs
+++ .original/roboform/src/Vault.cs
@@ -12,6 +12,17 @@ namespace RoboForm
             return Open(username, password, ui, new HttpClient());
         }
 
+        // Generates a random device id that should be used with every new device.
+        // Don't generate a new id at every login. This will prevent "remember this
+        // device" feature with two-factor authentication from working properly. Using
+        // a unique id every time also pollutes the server list of known devices. It
+        // might start refusing new connections at some point. This is not proven but
+        // easily possible as this happens with some other similar services.
+        public static string GenerateRandomDeviceId()
+        {
+            return Crypto.RandomDeviceId();
+        }
+
         //
         // Internal
         //
