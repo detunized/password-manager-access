381eae12b33274c18979ababede626935a5162a2
Dmitry Yakimenko <detunized@gmail.com>
2018-08-13 23:01:56 +0200

    opvault: KeyMac class to hold key-hmac pair

---
+++ .original/opvault/src/KeyMac.cs
@@ -0,0 +1,29 @@
+// Copyright (C) 2018 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using System;
+using System.Linq;
+
+namespace OPVault
+{
+    // Represents a key and [H]MAC key pair. They always go together since all the encryption is
+    // authenticated: every times something is encrypted a MAC/tag is added.
+    internal class KeyMac
+    {
+        public readonly byte[] Key;
+        public readonly byte[] MacKey;
+
+        public KeyMac(byte[] buffer)
+        {
+            if (buffer.Length != 64)
+                throw new InvalidOperationException("Buffer must be exactly 64 bytes long");
+
+            Key = buffer.Take(32).ToArray();
+            MacKey = buffer.Skip(32).ToArray();
+        }
+
+        public KeyMac(string base64): this(base64.Decode64())
+        {
+        }
+    }
+}
---
+++ .original/opvault/test/KeyMacTest.cs
@@ -0,0 +1,38 @@
+// Copyright (C) 2018 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using NUnit.Framework;
+
+namespace OPVault.Test
+{
+    [TestFixture]
+    public class KeyMacTest
+    {
+        [Test]
+        public void KeyMac_created_from_bytes()
+        {
+            var key = new KeyMac(Buffer.Decode64());
+
+            Assert.That(key.Key, Is.EqualTo(Key.Decode64()));
+            Assert.That(key.MacKey, Is.EqualTo(MacKey.Decode64()));
+        }
+
+        [Test]
+        public void KeyMac_created_from_base64()
+        {
+            var key = new KeyMac(Buffer);
+
+            Assert.That(key.Key, Is.EqualTo(Key.Decode64()));
+            Assert.That(key.MacKey, Is.EqualTo(MacKey.Decode64()));
+        }
+
+        //
+        // Data
+        //
+
+        private const string Buffer = "a7HZUoTh0E9I7LCTF3AHDRQXGEbcnQuUMv6Vcvv7e13IOFMfmCJORzuf" +
+                                      "hnDVeB4cDrxnTsPFYMTvpHboE8MPGg==";
+        private const string Key = "a7HZUoTh0E9I7LCTF3AHDRQXGEbcnQuUMv6Vcvv7e10=";
+        private const string MacKey = "yDhTH5giTkc7n4Zw1XgeHA68Z07DxWDE76R26BPDDxo=";
+    }
+}
