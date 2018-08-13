d7fef40879b202361666a93f0f555f885edf96c1
Dmitry Yakimenko <detunized@gmail.com>
2018-08-13 22:24:22 +0200

    opvault: string and JToken extension methods

---
+++ .original/opvault/src/Extensions.cs
@@ -0,0 +1,131 @@
+// Copyright (C) 2018 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using System;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace OPVault
+{
+    internal static class Extensions
+    {
+        //
+        // string
+        //
+
+        public static byte[] ToBytes(this string s)
+        {
+            return Encoding.UTF8.GetBytes(s);
+        }
+
+        public static byte[] Decode64(this string s)
+        {
+            return Convert.FromBase64String(s);
+        }
+
+        //
+        // Nested JToken access by path with and without exceptions
+        //
+
+        public static JToken At(this JToken j, string path)
+        {
+            var c = j;
+            foreach (var i in path.Split('/'))
+            {
+                if (c.Type != JTokenType.Object)
+                    throw new JTokenAccessException(
+                        string.Format("Expected nested objects at '{0}'", path));
+
+                c = c[i];
+                if (c == null)
+                    throw new JTokenAccessException(string.Format("Path '{0}' doesn't exist", path));
+            }
+
+            return c;
+        }
+
+        public static JToken At(this JToken j, string path, JToken defaultValue)
+        {
+            try
+            {
+                return j.At(path);
+            }
+            catch (JTokenAccessException)
+            {
+                return defaultValue;
+            }
+        }
+
+        public static string StringAt(this JToken j, string path)
+        {
+            var s = j.At(path);
+            if (s.Type != JTokenType.String)
+                throw new JTokenAccessException(string.Format("Expected a string at '{0}'", path));
+
+            return (string)s;
+        }
+
+        public static string StringAt(this JToken j, string path, string defaultValue)
+        {
+            try
+            {
+                return j.StringAt(path);
+            }
+            catch (JTokenAccessException)
+            {
+                return defaultValue;
+            }
+        }
+
+        public static int IntAt(this JToken j, string path)
+        {
+            var i = j.At(path);
+            if (i.Type != JTokenType.Integer)
+                throw new JTokenAccessException(string.Format("Expected an integer at '{0}'", path));
+
+            return (int)i;
+        }
+
+        public static int IntAt(this JToken j, string path, int defaultValue)
+        {
+            try
+            {
+                return j.IntAt(path);
+            }
+            catch (JTokenAccessException)
+            {
+                return defaultValue;
+            }
+        }
+
+        public static bool BoolAt(this JToken j, string path)
+        {
+            var b = j.At(path);
+            if (b.Type != JTokenType.Boolean)
+                throw new JTokenAccessException(string.Format("Expected a boolean at '{0}'", path));
+
+            return (bool)b;
+        }
+
+        public static bool BoolAt(this JToken j, string path, bool defaultValue)
+        {
+            try
+            {
+                return j.BoolAt(path);
+            }
+            catch (JTokenAccessException)
+            {
+                return defaultValue;
+            }
+        }
+    }
+
+    // TODO: Move this out of here!
+    internal class JTokenAccessException: Exception
+    {
+        public JTokenAccessException(string message)
+            : base(message)
+        {
+        }
+    }
+}
