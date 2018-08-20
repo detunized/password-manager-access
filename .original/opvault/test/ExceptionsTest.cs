ec30ff0c258b1059e0190338d1f048024c1411ae
Dmitry Yakimenko <detunized@gmail.com>
2018-08-20 10:33:15 +0200

    opvault: Base exception

---
+++ .original/opvault/test/ExceptionsTest.cs
@@ -0,0 +1,35 @@
+// Copyright (C) 2018 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using System;
+using NUnit.Framework;
+
+namespace OPVault.Test
+{
+    [TestFixture]
+    public class ExceptionsTest
+    {
+        [Test]
+        public void BaseException_properties_are_set()
+        {
+            VerifyException(new BaseException(Message, InnerException));
+        }
+
+        //
+        // Data
+        //
+
+        private const string Message = "message";
+        private static readonly Exception InnerException = new Exception();
+
+        //
+        // Helpers
+        //
+
+        private static void VerifyException(BaseException e)
+        {
+            Assert.That(e.Message, Is.EqualTo(Message));
+            Assert.That(e.InnerException, Is.SameAs(InnerException));
+        }
+    }
+}
