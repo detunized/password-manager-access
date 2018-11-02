acc2e8f2fa7d11937f1748485eb02bc5427b9a6b
Dmitry Yakimenko <detunized@gmail.com>
2018-11-02 13:10:09 +0100

    bitwarden: Read credentials from a file in the example program

--- .original/bitwarden/example/Program.cs
+++ .original/bitwarden/example/Program.cs
@@ -1,12 +1,23 @@
 // Copyright (C) 2018 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
+using System.IO;
+using Bitwarden;
+
 namespace Example
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Read Bitwarden credentials from a file
+            // The file should contain 2 lines:
+            //   - username
+            //   - password
+            // See credentials.txt.example for an example.
+            var credentials = File.ReadAllLines("../../credentials.txt");
+            var username = credentials[0];
+            var password = credentials[1];
         }
     }
 }
