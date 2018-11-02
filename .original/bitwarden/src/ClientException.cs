6a6b8e2ffc7d8ae6a88712693db7d116f29f5624
Dmitry Yakimenko <detunized@gmail.com>
2018-11-02 12:50:02 +0100

    bitwarden: Base and Client exception classes

---
+++ .original/bitwarden/src/ClientException.cs
@@ -0,0 +1,34 @@
+// Copyright (C) 2018 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using System;
+
+namespace Bitwarden
+{
+    public class ClientException: BaseException
+    {
+        public enum FailureReason
+        {
+            IncorrectCredentials,
+            NetworkError,
+            InvalidResponse,
+            RespondedWithError,
+            UnsupportedFeature,
+            InvalidOperation,
+            UnknownError
+        }
+
+        public ClientException(FailureReason reason, string message):
+            this(reason, message, null)
+        {
+        }
+
+        public ClientException(FailureReason reason, string message, Exception innerException):
+            base(message, innerException)
+        {
+            Reason = reason;
+        }
+
+        public FailureReason Reason { get; private set; }
+    }
+}
