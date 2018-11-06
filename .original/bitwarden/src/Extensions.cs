bf3b4275a08142d5a283c22ff681e6a27924307f
Dmitry Yakimenko <detunized@gmail.com>
2018-11-06 18:19:07 +0100

    bitwarden: Extension methods for string and byte[]

---
+++ .original/bitwarden/src/Extensions.cs
@@ -0,0 +1,34 @@
+// Copyright (C) 2018 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using System;
+using System.Text;
+
+namespace Bitwarden
+{
+    internal static class Extensions
+    {
+        //
+        // string
+        //
+
+        public static byte[] ToBytes(this string s)
+        {
+            return Encoding.UTF8.GetBytes(s);
+        }
+
+        public static byte[] Decode64(this string s)
+        {
+            return Convert.FromBase64String(s);
+        }
+
+        //
+        // byte[]
+        //
+
+        public static string ToUtf8(this byte[] x)
+        {
+            return Encoding.UTF8.GetString(x);
+        }
+    }
+}
