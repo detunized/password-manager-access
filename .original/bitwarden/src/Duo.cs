91683efdc68a645e469731b7b1a84a454ca588c3
Dmitry Yakimenko <detunized@gmail.com>
2018-12-22 21:44:47 +0100

    bitwarden: Download the Duo frame

--- .original/bitwarden/src/Duo.cs
+++ .original/bitwarden/src/Duo.cs
@@ -1,13 +1,28 @@
 // Copyright (C) 2018 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
+using HtmlAgilityPack;
+
 namespace Bitwarden
 {
     internal static class Duo
     {
         public static string Authenticate(Response.InfoDuo info, Ui ui)
         {
+            var html = DownloadFrame(info);
             return "";
+        }
+
+        internal static HtmlDocument DownloadFrame(Response.InfoDuo info)
+        {
+            const string parent = "https%3A%2F%2Fvault.bitwarden.com%2F%23%2F2fa";
+            const string version = "2.6";
+
+            var tx = info.Signature.Split(':')[0];
+            var url = $"https://{info.Host}/frame/web/v1/auth?tx={tx}&parent={parent}&v={version}";
+
+            // TODO: Better would be to use the IHttpClient here but it doesn't support redirects ATM
+            return new HtmlWeb() { CaptureRedirect = true }.Load(url, "POST");
         }
     }
 }
