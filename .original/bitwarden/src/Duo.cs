5611bdfa8b80a265678449f96ef92e3ea246a10d
Dmitry Yakimenko <detunized@gmail.com>
2018-12-22 21:57:41 +0100

    bitwarden: Parse the Duo html frame

--- .original/bitwarden/src/Duo.cs
+++ .original/bitwarden/src/Duo.cs
@@ -1,6 +1,8 @@
 // Copyright (C) 2018 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
+using System;
+using System.Linq;
 using HtmlAgilityPack;
 
 namespace Bitwarden
@@ -10,6 +12,20 @@ namespace Bitwarden
         public static string Authenticate(Response.InfoDuo info, Ui ui)
         {
             var html = DownloadFrame(info);
+
+            // Find the main form
+            var form = html.DocumentNode.SelectSingleNode("//form[@id='login-form']");
+            if (form == null)
+                throw new InvalidOperationException("Duo HTML: main form is not found");
+
+            // Find all the devices and the signature
+            var sid = GetInputValue(form, "sid");
+            var url = GetInputValue(form, "url");
+            var devices = GetDevices(form);
+
+            if (sid == null || url == null || devices == null)
+                throw new InvalidOperationException("Duo HTML: signature, url or devices are not found");
+
             return "";
         }
 
@@ -23,6 +39,51 @@ namespace Bitwarden
 
             // TODO: Better would be to use the IHttpClient here but it doesn't support redirects ATM
             return new HtmlWeb() { CaptureRedirect = true }.Load(url, "POST");
+        }
+
+        internal static string GetInputValue(HtmlNode form, string name)
+        {
+            return form
+                .SelectSingleNode($"./input[@name='{name}']")?
+                .Attributes["value"]?
+                .DeEntitizeValue;
+        }
+
+        internal struct Device
+        {
+            public readonly string Id;
+            public readonly string Name;
+            public readonly string[] Factors;
+
+            public Device(string id, string name, string[] factors)
+            {
+                Id = id;
+                Name = name;
+                Factors = factors;
+            }
+        }
+
+        internal static Device[] GetDevices(HtmlNode form)
+        {
+            var devices = form
+                .SelectNodes("//select[@name='device']/option")?
+                .Select(x => (Id: x.Attributes["value"]?.DeEntitizeValue,
+                              Name: HtmlEntity.DeEntitize(x.InnerText ?? "")));
+
+            if (devices == null || devices.Any(x => x.Id == null || x.Name == null))
+                return null;
+
+            return devices.Select(x => new Device(x.Id, x.Name, GetDeviceFactors(form, x.Id))).ToArray();
+        }
+
+        private static string[] GetDeviceFactors(HtmlNode form, string id)
+        {
+            return form
+                .SelectSingleNode($".//fieldset[@data-device-index='{id}']")?
+                .SelectNodes(".//input[@name='factor']")?
+                .Select(x => x.Attributes["value"]?.DeEntitizeValue)?
+                .Where(x => x != null)?
+                .ToArray() ?? new string[0];
         }
     }
 }
