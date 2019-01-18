0a47bac774c3f734f54bb829400d4202277f71f7
Dmitry Yakimenko <detunized@gmail.com>
2019-01-18 13:06:19 +0100

    bitwarden: Parse PKCS8 private RSA key

--- .original/bitwarden/src/Crypto.cs
+++ .original/bitwarden/src/Crypto.cs
@@ -1,6 +1,7 @@
 // Copyright (C) 2018 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
+using System;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
@@ -59,6 +60,58 @@ namespace Bitwarden
             {
                 throw new ClientException(ClientException.FailureReason.CryptoError, "Decryption failed", e);
             }
+        }
+
+        public static RSAParameters ParsePrivateKeyPkcs8(byte[] asn1)
+        {
+            // See: https://tools.ietf.org/html/rfc5208
+            var privateKeyInfo = ExtractAsn1Item(asn1, Asn1.Kind.Sequence);
+            var privateKey = privateKeyInfo.Open(reader => {
+                ExtractAsn1Item(reader, Asn1.Kind.Integer); // Discard the version
+                ExtractAsn1Item(reader, Asn1.Kind.Sequence); // Discard the algorithm
+                return ExtractAsn1Item(reader, Asn1.Kind.OctetString);
+            });
+            var berEncodedPrivateKey = ExtractAsn1Item(privateKey, Asn1.Kind.Sequence);
+
+            // See: https://tools.ietf.org/html/rfc3447#appendix-C
+            return berEncodedPrivateKey.Open(reader => {
+                ExtractAsn1Item(reader, Asn1.Kind.Integer); // Discard the version
+
+                // There are occasional leading zeros that must be stripped
+                Func<byte[]> readInteger =
+                    () => ExtractAsn1Item(reader, Asn1.Kind.Integer).SkipWhile(i => i == 0).ToArray();
+
+                return new RSAParameters
+                {
+                    Modulus = readInteger(),
+                    Exponent = readInteger(),
+                    D = readInteger(),
+                    P = readInteger(),
+                    Q = readInteger(),
+                    DP = readInteger(),
+                    DQ = readInteger(),
+                    InverseQ = readInteger()
+                };
+            });
+        }
+
+        //
+        // Internal
+        //
+
+        internal static byte[] ExtractAsn1Item(byte[] bytes, Asn1.Kind expectedKind)
+        {
+            return bytes.Open(reader => ExtractAsn1Item(reader, expectedKind));
+        }
+
+        internal static byte[] ExtractAsn1Item(BinaryReader reader, Asn1.Kind expectedKind)
+        {
+            var item = Asn1.ExtractItem(reader);
+            if (item.Key != expectedKind)
+                throw new ClientException(ClientException.FailureReason.InvalidFormat,
+                                          $"ASN1 decoding failed, expected {expectedKind}, got {item.Key}");
+
+            return item.Value;
         }
     }
 }
--- .original/bitwarden/test/CryptoTest.cs
+++ .original/bitwarden/test/CryptoTest.cs
@@ -74,6 +74,42 @@ namespace Bitwarden.Test
                             .And.InnerException.InstanceOf<CryptographicException>());
         }
 
+        [Test]
+        public void ParsePrivateKeyPkcs8_parses_openssl_generated_key()
+        {
+            // Generate with
+            // $ openssl genpkey -out rsakey.pem -algorithm RSA -pkeyopt rsa_keygen_bits:2048
+            var key = "MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDl3KLYUotetBFN" +
+                      "uzuCSQkUKIC/S7U1F7zjy2EzhbqRSQPi6TsCpDz+kal28hcXiPH1e3KlfeObmqoK" +
+                      "8/OUdOtQzBqd5E8egoGBT3MKgKbJdFqQjI7hEAEX1E4S4WdM0wjLbaJ83PE1si1J" +
+                      "eiEllBHrSnvV/Vgzrqx+j9zthUlmxTorhPj0ZPaz81q+ZFpBoqGw82V1D8Z0mlW9" +
+                      "X3qxnSmXXXcx2VbPBbKbI4sHkL1KMZenvcofHqOzsu9KqcNyYyOvzgQabwnfyTvv" +
+                      "p3fSu5WpNKDOxS3+HiPDkSwcf7ZggsOcGaZpTGebaDV9wG8FsUM/MhGQwjJsaAch" +
+                      "C3R/KhbPAgMBAAECggEAcMaRdwT4sBw0qSiFh+SBecxtbm2cmFHvMOwl4ntoD8P9" +
+                      "DiVT+HXQHy4kMOGSBs//tms80okzVLLLXthV166NjXS2UmUjlFp/Y4wxayO3sPtP" +
+                      "O8BakX2iq6hULds9LWoU1IoTwKM+DRRAN96dyKwfJovCuji0P5chtm6q/DX6z1pi" +
+                      "hnCfo3lFySKmEif/D55WbioVezRoDmBDw2zzxAl/OzVn37hVaxmnKzT5w6tJYmHQ" +
+                      "ctilFMlKYQ6O+IUSphylwsD+P3Xszp1Am9SJGbIuDi63NW+5auwDdL40AjaIzXcU" +
+                      "NoSmivEz/bMfIUeEeIcWM3Ph4Pmaly+Vdy7daX0YIQKBgQD66i6ChbYElRRojD2/" +
+                      "GB9+jY2hQOe8Y8b75Lq2TNyNtB1wOWr0E04Ab2TitqW0SQVZZwUA4r0M3/leAxCj" +
+                      "w1dmNlnC6obFgixBr3vMhL42xzxGam46887m8Hp3zR9WV2RJqOUkY58WReQ5d4ZR" +
+                      "pwN2JCUMdIqTJFblrzlIbNDcxwKBgQDqhTnQ3ELAnJEF/4emO2IQZHjkqUYPjZUn" +
+                      "NmD4zi/W6PYmbRF2uLs6lFwyEGNkO3FcIr7OQZniijcTRPyCFKyllAYzRDlg6Xe5" +
+                      "GXvN1zDyyr9qfUCWDv7aec8fcG3Wlcl64HTYhWqTUDRICg80B0dwomYZwYmtw/cb" +
+                      "TtnDSwgduQKBgQDwBrfJSmnNxmMIhCfJNyVhpb33pSRJtlUywCLQo4RU1hlXMsYa" +
+                      "iKEUj3s92aV4amYAmSlTKJPaziM5iMsRLZvc/l8ts7aLGDSH/Xv7MHzZ4AvL/KJI" +
+                      "KNUjXKZ7FjFFIkKgqD3Taq1T3DYvtyQ51f/cs0TxvkaV7axm+hFdNCM1HwKBgQDE" +
+                      "drNGix7IqOC8+6Ix2hF+1gyI0l3E7cEDxoRaKrDdAvAipMA6iuseWecacawx7bI7" +
+                      "k0fxFffKVitpUyON/a/cTjMbg43l5+/J+tVCTYHOA5dzqFYQ36MUd3LSTadWWskc" +
+                      "zShVsv7jRg9mQjcWSPrKGuIZtY6XKVG7aMT428SC0QKBgQDawNe6yb6B+Bl/iS7Y" +
+                      "tLl2Z4I8xWlkzCvClofMmzWNiOPCWehXf24iergVK4HUnelCVB0XQSDK4perAh8g" +
+                      "IYR4hTuct+gYQhFtQCJvXcxeElHS7tElKYZq5QL1e5weTJ4wX+aOYqJA/V4lrmE+" +
+                      "Nmo9sez/pKeLnCqra8Ew29paWg==";
+
+            // Check nothing here. Not throwing anything is good enough.
+            Crypto.ParsePrivateKeyPkcs8(key.Decode64());
+        }
+
         //
         // Data
         //
