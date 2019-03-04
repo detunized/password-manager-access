4c195d37a228d62ff37f76216268f84348036044
Dmitry Yakimenko <detunized@gmail.com>
2019-03-04 16:21:33 +0100

    Refactor exceptions: ClientException removed

--- test/Common/Exceptions.cs
+++ test/Common/Exceptions.cs
@@ -8,39 +8,6 @@ namespace PasswordManagerAccess.Common
 {
     internal static class Exceptions
     {
-        public static ClientException AssertThrowsClientException(Action action)
-        {
-            var e = Record.Exception(action);
-
-            Assert.NotNull(e);
-            Assert.IsType<ClientException>(e);
-
-            return (ClientException)e;
-        }
-
-        public static ClientException AssertThrowsClientException(Action action,
-                                                                  ClientException.FailureReason reason,
-                                                                  string message = "")
-        {
-            var e = AssertThrowsClientException(action);
-
-            Assert.Equal(reason, e.Reason);
-            Assert.NotNull(e.Message);
-            Assert.Contains(message, e.Message);
-
-            return e;
-        }
-
-        public static ClientException AssertThrowsInvalidResponse(Action action, string message = "")
-        {
-            return AssertThrowsClientException(action, ClientException.FailureReason.InvalidResponse, message);
-        }
-
-        public static ClientException AssertThrowsInvalidOperation(Action action, string message = "")
-        {
-            return AssertThrowsClientException(action, ClientException.FailureReason.InvalidOperation, message);
-        }
-
         public static InternalErrorException AssertThrowsInternalError(Action action, string message = "")
         {
             var e = Assert.Throws<InternalErrorException>(action);
