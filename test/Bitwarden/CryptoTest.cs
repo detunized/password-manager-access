43522b5b58c26b26d70084453f44937a0dd7059f
Dmitry Yakimenko <detunized@gmail.com>
2019-03-25 23:22:19 +0100

    Updated comments

--- test/Bitwarden/CryptoTest.cs
+++ test/Bitwarden/CryptoTest.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2018 Dmitry Yakimenko (detunized@gmail.com).
+// Copyright (C) 2012-2019 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
 using System.Security.Cryptography;
