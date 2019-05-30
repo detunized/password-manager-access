4179065004899bdef06c86caa983b1dea1745b2d
Dmitry Yakimenko <detunized@gmail.com>
2019-05-30 14:31:14 +0200

    ZohoVault working example

--- example/ZohoVault/Program.cs
+++ example/ZohoVault/Program.cs
@@ -1,15 +1,48 @@
-﻿// Copyright (C) 2012-2019 Dmitry Yakimenko (detunized@gmail.com).
+// Copyright (C) 2012-2019 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
 using System;
+using PasswordManagerAccess.Common;
+using PasswordManagerAccess.ZohoVault;
+using PasswordManagerAccess.Example.Common;
 
-namespace ZohoVault
+namespace PasswordManagerAccess.Example.ZohoVault
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = Util.ReadConfig();
+            try
+            {
+                // Open the remote vault
+                var vault = Vault.Open(config["username"], config["password"], config["passphrase"]);
+
+                // Print the decrypted accounts
+                for (int i = 0; i < vault.Accounts.Length; ++i)
+                {
+                    var account = vault.Accounts[i];
+
+                    Console.WriteLine("{0}:\n" +
+                                      "          id: {1}\n" +
+                                      "        name: {2}\n" +
+                                      "    username: {3}\n" +
+                                      "    password: {4}\n" +
+                                      "         url: {5}\n" +
+                                      "        note: {6}\n",
+                                      i + 1,
+                                      account.Id,
+                                      account.Name,
+                                      account.Username,
+                                      account.Password,
+                                      account.Url,
+                                      account.Note);
+                }
+            }
+            catch (BaseException e)
+            {
+                Util.PrintException(e);
+            }
         }
     }
 }
