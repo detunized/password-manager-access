13a5cf80ab0eea9d51e9c8493aa40614ac97c186
Dmitry Yakimenko <detunized@gmail.com>
2019-05-30 14:18:56 +0200

    ZohoVault example embryo

---
+++ example/ZohoVault/Program.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (C) 2012-2019 Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using System;
+
+namespace ZohoVault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
