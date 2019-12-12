8d169dafa2aa6d9e6764f89a54428cb67f731d17
Dmitry Yakimenko <detunized@gmail.com>
2019-12-12 10:22:26 +0100

    1Password: copyright updated

--- test/OnePassword/RsaKeyTest.cs
+++ test/OnePassword/RsaKeyTest.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
+// Copyright (C) 2012-2019 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
 using System.Security.Cryptography;
