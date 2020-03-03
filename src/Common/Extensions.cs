6ece09a8d41b4c75db4358e32764e92f14f85388
Dmitry Yakimenko <detunized@gmail.com>
2020-03-03 19:34:07 +0100

    Common: JToken extensions moved from RoboForm to Common

--- src/Common/Extensions.cs
+++ src/Common/Extensions.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace PasswordManagerAccess.Common
 {
@@ -318,6 +319,34 @@ namespace PasswordManagerAccess.Common
                          ((result & 0xFF000000) >> 24);
 
             return result;
+        }
+
+        //
+        // JToken
+        //
+
+        public static string StringAt(this JToken j, string name, string defaultValue)
+        {
+            return At(j, name, JTokenType.String, defaultValue);
+        }
+
+        public static int IntAt(this JToken j, string name, int defaultValue)
+        {
+            return At(j, name, JTokenType.Integer, defaultValue);
+        }
+
+        public static bool BoolAt(this JToken j, string name, bool defaultValue)
+        {
+            return At(j, name, JTokenType.Boolean, defaultValue);
+        }
+
+        private static T At<T>(JToken j, string name, JTokenType type, T defaultValue)
+        {
+            if (j?.Type == JTokenType.Object)
+                if (j[name] is var field && field?.Type == type)
+                    return field.ToObject<T>();
+
+            return defaultValue;
         }
     }
 }
