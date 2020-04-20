70c06c2b4a558579f2c1f14f518fa04484bd4603
Dmitry Yakimenko <detunized@gmail.com>
2020-04-20 10:21:25 +0200

    Project: fixed the header template for the new files

---
+++ src/TrueKey/Response.cs
@@ -0,0 +1,10 @@
+// Copyright (C) Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+namespace PasswordManagerAccess.TrueKey
+{
+    public class Response
+    {
+        
+    }
+}
