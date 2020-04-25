c6160550f32ae23cd1beeaf5b0e8516f744d0b5a
Dmitry Yakimenko <detunized@gmail.com>
2020-04-25 19:46:34 +0200

    LastPass: copyright header updated [skip ci]

--- test/Common/S3Test.cs
+++ test/Common/S3Test.cs
@@ -1,5 +1,5 @@
-// // Copyright (C) Dmitry Yakimenko (detunized@gmail.com).
-// // Licensed under the terms of the MIT license. See LICENCE for details.
+// Copyright (C) Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
 
 using System;
 using System.Collections.Generic;
