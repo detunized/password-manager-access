a0775aa2e0b38a469dd8380504765a60b2c533b5
Dmitry Yakimenko <detunized@gmail.com>
2020-06-02 13:00:41 +0200

    OpVault: the example program ported over from .original (WIP) [skip ci]

--- example/OpVault/Program.cs
+++ example/OpVault/Program.cs
@@ -1,26 +1,26 @@
-// Copyright (C) 2018 Dmitry Yakimenko (detunized@gmail.com).
+// Copyright (C) Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
 using System;
-using OPVault;
+using PasswordManagerAccess.Example.Common;
+using PasswordManagerAccess.OpVault;
 
-namespace Example
+namespace PasswordManagerAccess.Example.OpVault
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
-            // Use one from the tests by default
-            var path = "../../../test/test.opvault";
-            var password = "password";
+            var config = Util.ReadConfig();
 
-            if (args.Length == 2)
+            // Use one from the tests by default
+            if (!config.ContainsKey("path") || !config.ContainsKey("password"))
             {
-                path = args[0];
-                password = args[1];
+                config["path"] = "../../..";
+                config["password"] = "password";
             }
 
-            var accounts = Vault.Open(path, password);
+            var accounts = Vault.Open(config["path"], config["password"]);
             for (var i = 0; i < accounts.Length; ++i)
             {
                 var account = accounts[i];
