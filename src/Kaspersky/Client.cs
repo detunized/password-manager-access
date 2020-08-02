e40487863ba3cda15579be5f6c27ddfac15b4da8
Dmitry Yakimenko <detunized@gmail.com>
2020-08-02 18:12:42 +0200

    Kaspersky: lots of item parsing unfinished code (WIP)

--- src/Kaspersky/Client.cs
+++ src/Kaspersky/Client.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using PasswordManagerAccess.Common;
@@ -72,6 +73,7 @@ namespace PasswordManagerAccess.Kaspersky
             var db = bosh.GetChanges(GetDatabaseCommand, GetDatabaseCommandId, authKey.ToBase64());
 
             // TODO: Parse the db here
+            ParseVault(db);
         }
 
         //
@@ -252,34 +254,79 @@ namespace PasswordManagerAccess.Kaspersky
             return host.Substring(dot + 1);
         }
 
-        internal static object DecryptItem(Bosh.Change item)
+        internal static object[] ParseVault(IEnumerable<Bosh.Change> db)
+        {
+            var accounts = new List<Dictionary<string, string>>();
+            var credentials = new List<Dictionary<string, string>>();
+
+            foreach (var item in db)
+            {
+                switch (item.Type)
+                {
+                case "WebAccount":
+                    accounts.Add(ParseWebAccount(item));
+                    break;
+                case "Login":
+                    credentials.Add(ParseLogin(item));
+                    break;
+                }
+            }
+
+            return null;
+        }
+
+        internal static Dictionary<string, string> ParseWebAccount(Bosh.Change item)
+        {
+            var (version, blob) = DecodeItem(item);
+            return version switch
+            {
+                Version92 => ParseItemVersion92(blob, "m_guid", "m_name", "m_url", "m_comment"),
+                _ => throw new UnsupportedFeatureException($"Database item version {version} is not supported")
+            };
+        }
+
+        internal static Dictionary<string, string> ParseLogin(Bosh.Change item)
+        {
+            var (version, blob) = DecodeItem(item);
+            return version switch
+            {
+                Version92 => ParseItemVersion92(blob, "m_guid", "m_account", "m_name", "m_login", "m_password", "m_comment"),
+                _ => throw new UnsupportedFeatureException($"Database item version {version} is not supported")
+            };
+        }
+
+        internal static (int, byte[]) DecodeItem(Bosh.Change item)
         {
             var blob = item.Data.Decode64();
             if (blob.Length < 4)
-                throw MakeError("Database is corrupted: encrypted item is too short");
+                throw CorruptedError("encrypted item is too short");
 
-            var version = blob[0];
-            if (version == Version92)
-                return DecryptItemVersion92(blob);
-
-            throw new UnsupportedFeatureException($"Database item version {version} is not supported");
+            return (blob[0], blob);
         }
 
-        internal static object DecryptItemVersion92(byte[] blob)
+        internal static Dictionary<string, string> ParseItemVersion92(byte[] blob, params string[] names)
         {
+            if (blob.Length < 6)
+                throw CorruptedError("encrypted item is too short");
+
             using var inputStream = new MemoryStream(blob, false);
 
-            // Skip version (4 bytes) and Zlib header (2 bytes)
+            // Skip the item header:
+            //   - version (4 bytes)
+            //   - Zlib header (2 bytes)
             for (var i = 0; i < 6; i++)
                 inputStream.ReadByte();
 
             using var deflateStream = new DeflateStream(inputStream, CompressionMode.Decompress);
             var decompressed = deflateStream.ReadAll();
+            var json = decompressed.ToUtf8();
 
-            var json = JObject.Parse(decompressed.ToUtf8());
+            return ParseItemVersion92(json, names);
+        }
 
-            var fields = json["fields"];
-            var fieldProperties = json["attributes"]["propertiesMetadata"];
+        internal static Dictionary<string, string> ParseItemVersion92(string json, params string[] names)
+        {
+            var item = JObject.Parse(json);
 
             // Field types:
             //
@@ -299,21 +346,78 @@ namespace PasswordManagerAccess.Kaspersky
 
             // Fields could be encrypted or not, this is defined by the field attributes.
             // The stored type of the field is ignored by the parser and it uses a hardcoded
-            // table of known names and associated types.
+            // table of known names and associated types. All our supported types are strings
+            // and they are all seem to be encrypted. This makes things a lot easier. This
+            // might change in the future.
 
-            // Encrypted blob has a header of 12 words (48 bytes)
-            // 16 bytes of IV
-            // 32 bytes of HMAC
-            // The rest of the blob contains the ciphertext encrypted with AES-256-CBC with PKCS#7 padding.
+            var fields = item.ArrayAtOrEmpty("fields");
+            var fieldProperties = FindFieldByName(item.ArrayAtOrEmpty("attributes"), "propertiesMetadata")
+                .ObjectAtOrEmpty("data")
+                .ArrayAtOrEmpty("blob");
 
-            // For version 9.2 strings are stored in UTF-16. Otherwise it's UTF-8.
+            var result = new Dictionary<string, string>(names.Length);
+            foreach (var name in names)
+            {
+                // It's possible the field is not present at all.
+                var field = FindFieldByName(fields, name);
+                if (field == null)
+                    continue;
 
-            // Look for `function E(e, t, n) {` for more info.
+                // It's also possible that its data is either null or blank.
+                var fieldData = field.ObjectAtOrEmpty("data").StringAt("blob", null);
+                if (fieldData == null)
+                    continue;
 
-            // HMAC for versions 9+ are calculated on the encrypted data
-            // HMAC for version 8 is calculated on the decrypted string
+                // When it's present but blank we're done. It cannot be encrypted.
+                if (fieldData.Length == 0)
+                {
+                    result[name] = "";
+                    continue;
+                }
 
-            return null;
+                var properties = FindFieldByName(fieldProperties, name);
+                var isEncrypted = FindFieldByName(properties.ArrayAtOrEmpty("fields"), "isEncrypted")
+                    .ObjectAtOrEmpty("data")
+                    .BoolAt("blob", false);
+
+                // If the field is not encrypted we use its data blob as is.
+                if (!isEncrypted)
+                {
+                    result[name] = fieldData;
+                    continue;
+                }
+
+                // Otherwise it should a base64 encoded binary encrypted blob.
+                var blob = fieldData.Decode64();
+
+                // Encrypted blob has a header of 12 words (48 bytes)
+                // 16 bytes of IV
+                // 32 bytes of tag/MAC
+                // The rest of the blob contains the ciphertext encrypted with AES-256-CBC with PKCS#7 padding.
+                var iv = blob.Sub(0, 16);
+                var tag = blob.Sub(16, 32);
+                var ciphertext = blob.Sub(48, int.MaxValue);
+
+                // TODO: Verify tag/MAC
+                // MAC for versions 9+ is calculated on the encrypted data
+                // MAC for version 8 is calculated on the decrypted string
+                // Look for `function E(e, t, n) {` for more info.
+
+                // TODO: Pass this in
+                var key = "d8f2bfe4980d90e3d402844e5332859ecbda531ab24962d2fdad4d39ad98d2f9".DecodeHex();
+
+                var plaintext = Crypto.DecryptAes256Cbc(ciphertext, iv, key);
+
+                // For version 9.2 strings are stored in UTF-16. Otherwise it's UTF-8.
+                result[name] = Encoding.BigEndianUnicode.GetString(plaintext);
+            }
+
+            return result;
+        }
+
+        internal static JToken FindFieldByName(JArray fields, string name)
+        {
+            return fields.FirstOrDefault(x => x.StringAt("name", "") == name);
         }
 
         internal static BaseException MakeError(RestResponse<string> response)
@@ -328,6 +432,11 @@ namespace PasswordManagerAccess.Kaspersky
         internal static BaseException MakeError(string message, Exception inner = null)
         {
             return new InternalErrorException(message, inner);
+        }
+
+        internal static InternalErrorException CorruptedError(string message)
+        {
+            return new InternalErrorException($"Database is corrupted: {message}");
         }
 
         //
--- test/Kaspersky/ClientTest.cs
+++ test/Kaspersky/ClientTest.cs
@@ -1,13 +1,16 @@
 // Copyright (C) Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
 using PasswordManagerAccess.Common;
 using PasswordManagerAccess.Kaspersky;
 using Xunit;
 
 namespace PasswordManagerAccess.Test.Kaspersky
 {
-    public class ClientTest
+    public class ClientTest: TestBase
     {
         [Fact]
         public void GetHost_returns_domain()
@@ -38,12 +41,22 @@ namespace PasswordManagerAccess.Test.Kaspersky
         }
 
         [Fact]
+        public void ParseVault_returns_accounts()
+        {
+            var changes = XDocument.Parse(GetFixture("vault-response", "xml"))
+                .XPathSelectElements("//*[starts-with(local-name(), 'item_')]")
+                .Select(x => new Bosh.Change(x.Attribute("type").Value, x.Attribute("dataInBase64").Value));
+
+            Client.ParseVault(changes);
+        }
+
+        [Fact]
         public void DecryptItemVersion92_decrypts_item()
         {
             var blob =
                 "AwAAAHja7Vdbc6pIEP4vvOakRAXEU5WHGRiJERRUSMjWqS2EAUZuyjWa8r8fyG5ysoZUadXZWmsr88Clp7/u5usZpvuZsvI8Jasixxn1/Y9nyrFyi/r+TK3CZPUicAkOnba5Ljtkej2aZXp9nv1G5bsNroWHb1RsRfVTo42VxCG1AYc6fDuGu1aY4VdY7xeMZCLepNiu0W2wPC3aUSi2093mTJBBMrIKcQuEbvmiEqcZSWLq8ONNFP1pJ1GE4/zFxIVx9TnsvyLLwa5VhLmceCT+YuwUxryCOF9MncIUsZOvNXUaUxko8sSyc1JaOfli7VTWRlaZpCTH/x++Pkf9FsIkHOO0CXjayC6QtYurJuILJerytuPGSuuyS0qTYiNd6Bl5caurSMMvnk7jCWw24W7ZaDczf8HYX7BNmmxwmhOcKTi3Xtyc6e/f3pm+lYm1opDE+Wt7ciKBMrFxnJ2d4BCfmSjQFCD4LC+LXWz7aRKT/Xmu3uPekvrpLqBoEbE000XXiOHRNUP32GvID9G1KNC9LgBDBAcs9eqIPrSV68cW/x7XLZfX0W6x7T/324x/6MeOLEdppMwCj5b9K9XwOCGeaygfjB9mIVm6D3dw2N2P5YWapcqDVQbJfYDiUrGQat3Kt0wU6QWzlKaTcr3eFgQ+zhn+cRw42s3NWzT9Q9vh988o4iIMW/Xft95HgRMDzuYVPZG8BNRjutB9pHv1E2xegSYAs7m7fGfov0gepos5PQZpxtic1gjmsaZ3a23haV2VvKnpjfDORv6jXQEgZkr9OhCB7ZRbqQEI+kKfQ0P0VfMKbiw3VAXLKoCdDjvuEJqqfBfsyISXpAzWIZExnflVptqe2uuvyr7a66SGr005SEpgo9QIINvpdFxXVaQ9cu5FfoxKE8w0YPrcDm3FWdofmLNJECEwQbQHuPFUk00DBgIk2wr7t8jywBPIyd1AmEdrCYjCYjd0vGgrAqny10z1JALoB1IfsR7OoC+o9x2mmQM4YaVZCcUXXlA4WgaLQosEoT1pb23XUQZmrJnQoy2ZF9vAXctKWATG4H7EArFm0ZNCutjzworIcFcsO7DiSpX0VQ7uc/So9HtjnjdZp2QLYc9Bg1T9ACTTTliJc0GQsBKb3K292ju23zUG2ifL6fW0OwpsrpZOJrGok0yJP7EKcxLoOpRHm2UVZRF0ASsNLdm7c+HUNhB0XS4okkCccWNVSW93+ZWv6xwS781tOpIfB7qzZhZ5dfN5EO+OkpN/dR/ajdNhH4vu07HHfeGPw0+s9YS/";
 
-            Client.DecryptItemVersion92(blob.Decode64());
+            Client.ParseItemVersion92(blob.Decode64());
         }
     }
 }
