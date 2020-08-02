65f98e9f290511085d2b58df1b2edd1debad3f90
Dmitry Yakimenko <detunized@gmail.com>
2020-08-02 19:37:20 +0200

    Kaspersky: Account class to store the parsed data

---
+++ src/Kaspersky/Account.cs
@@ -0,0 +1,41 @@
+// Copyright (C) Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+namespace PasswordManagerAccess.Kaspersky
+{
+    public class Account
+    {
+        public readonly string Id;
+        public readonly string Name;
+        public readonly string Url;
+        public readonly string Notes;
+        public readonly Credentials[] Credentials;
+
+        public Account(string id, string name, string url, string notes, Credentials[] credentials)
+        {
+            Id = id;
+            Name = name;
+            Url = url;
+            Notes = notes;
+            Credentials = credentials;
+        }
+    }
+
+    public class Credentials
+    {
+        public readonly string Id;
+        public readonly string Name;
+        public readonly string Username;
+        public readonly string Password;
+        public readonly string Notes;
+
+        public Credentials(string id, string name, string username, string password, string notes)
+        {
+            Id = id;
+            Name = name;
+            Username = username;
+            Password = password;
+            Notes = notes;
+        }
+    }
+}
