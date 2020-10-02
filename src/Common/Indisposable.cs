5fcc2ce71ecf0514b03765302b6bca9792df8c70
Dmitry Yakimenko <detunized@gmail.com>
2020-10-02 14:55:45 +0200

    Common: Indisposable class to wrap a regular class in the IDisposable interface

---
+++ src/Common/Indisposable.cs
@@ -0,0 +1,28 @@
+// Copyright (C) Dmitry Yakimenko (detunized@gmail.com).
+// Licensed under the terms of the MIT license. See LICENCE for details.
+
+using System;
+
+namespace PasswordManagerAccess.Common
+{
+    // This is an IDisposable wrapper for any regular non-disposable class so it could be
+    // assigned to "using var" variable. It could be used to mix disposable and regular objects.
+    // For example:
+    // using IDisposable obj = isRegular ? new Indisposable<Regular>(new Regular()) : new Disposable();
+    internal readonly struct Indisposable<T>: IDisposable
+    {
+        public T Value { get; }
+
+        public Indisposable(T t)
+        {
+            Value = t;
+        }
+
+        public static implicit operator T(Indisposable<T> i) => i.Value;
+
+        public void Dispose()
+        {
+            // Nothing to dispose of
+        }
+    }
+}
