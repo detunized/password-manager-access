991446f1954e97b3c212f57eacbf4796fa912fc8
Dmitry Yakimenko <detunized@gmail.com>
2021-04-13 17:24:42 +0200

    Kdbx: added missing public interface to the Parse overloads

--- src/Kdbx/Vault.cs
+++ src/Kdbx/Vault.cs
@@ -1,15 +1,27 @@
 // Copyright (C) Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
+using System.IO;
+
 namespace PasswordManagerAccess.Kdbx
 {
     public class Vault
     {
         public readonly Account[] Accounts;
 
-        public static Vault Open(string filename, string password, string keyfile)
+        public static Vault Open(string filename, string password, string keyfile = null)
         {
             return new Vault(Parser.Parse(filename, password, keyfile));
+        }
+
+        public static Vault Open(byte[] input, string password, byte[] keyfile = null)
+        {
+            return new Vault(Parser.Parse(input, password, keyfile));
+        }
+
+        public static Vault Open(Stream input, string password, Stream keyfile = null)
+        {
+            return new Vault(Parser.Parse(input, password, keyfile));
         }
 
         //
