68037740acc7bed10be33cf0dbf8985d4f441adb
Dmitry Yakimenko <detunized@gmail.com>
2021-04-14 21:43:04 +0200

    LastPass: GUI (WIP)

--- example/LastPassGui/Form1.Designer.cs
+++ example/LastPassGui/Form1.Designer.cs
@@ -28,12 +28,34 @@ namespace LastPassGui
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(800, 450);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
     }
 }
--- example/LastPassGui/Form1.cs
+++ example/LastPassGui/Form1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows.Forms;
 
 namespace LastPassGui
@@ -7,6 +8,30 @@ namespace LastPassGui
         public Form1()
         {
             InitializeComponent();
+
+            listView1.Columns.Add("Name");
+            listView1.Columns.Add("Username");
+            listView1.Columns.Add("Password");
+            listView1.Columns.Add("Url");
         }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+
+            if (!_loggedIn)
+            {
+                MessageBox.Show(this, "Please login");
+                _loggedIn = true;
+
+                listView1.Items.Add("test");
+            }
+        }
+
+        //
+        // Data
+        //
+
+        private bool _loggedIn = false;
     }
 }
