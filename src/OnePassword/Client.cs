b926be9e94f52d0136327a526f2a39ff8d285b49
Dmitry Yakimenko <detunized@gmail.com>
2022-01-14 17:06:15 +0100

    1Password: bump the client version to 1179

--- src/OnePassword/Client.cs
+++ src/OnePassword/Client.cs
@@ -15,7 +15,7 @@ namespace PasswordManagerAccess.OnePassword
     {
         public const string DefaultDomain = "my.1password.com";
         public const string ClientName = "1Password for Web";
-        public const string ClientVersion = "1172"; // TODO: This needs to be updated every now and then.
+        public const string ClientVersion = "1179"; // TODO: This needs to be updated every now and then.
         public const string ClientId = ClientName + "/" + ClientVersion;
 
         public enum Region
