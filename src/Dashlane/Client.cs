f7e58e3aa0fbaa8bb8af1d26c10c0943c9e17ab2
Dmitry Yakimenko <detunized@gmail.com>
2022-09-26 13:38:13 +0200

    Dashlane: lots of desktop protocol code removed

--- src/Dashlane/Client.cs
+++ src/Dashlane/Client.cs
@@ -1,7 +1,6 @@
 // Copyright (C) Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
-using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -11,203 +10,15 @@ namespace PasswordManagerAccess.Dashlane
 {
     using R = Response;
 
+    // TODO: Merge this with ClientWeb
     internal static class Client
     {
-        // TODO: Don't return JObject
-        public static R.Vault OpenVault(string username, string deviceId, Ui ui, IRestTransport transport)
+        internal static R.Vault Fetch(string username, string deviceId, IRestTransport transport)
         {
             var rest = new RestClient(transport, BaseApiUrl);
 
-            var loginType = RequestLoginType(username, rest);
-            if (loginType == LoginType.DoesntExist)
-                throw new BadCredentialsException("Invalid username");
-
-            var registered = IsDeviceRegistered(username, deviceId, rest);
-
-            // We have a registered device, no 2FA code is needed unless always-OTP mode is on. In
-            // always-OTP mode registering a device doesn't stop the server from asking OTP the next
-            // time around. So remember-me function is this mode is more or less useless. The
-            // registered device shows up in the admin area though.
-            if (registered && loginType != LoginType.GoogleAuth_Always)
-                return Fetch_TODO_remove(username, deviceId, rest);
-
-            // Try to fetch a few times and then register the device
-            var attempt = 0;
-            while (true)
-            {
-                try
-                {
-                    var passcode = GetPasscodeFromUser(username, loginType, attempt++, ui, rest);
-                    var blob = Fetch(username, loginType, passcode.Code, rest);
-
-                    if (passcode.RememberMe && !registered)
-                    {
-                        var token = blob.Token ?? passcode.Code;
-                        RegisterDeviceWithToken(username, deviceId, DeviceName, token, rest);
-                    }
-
-                    return blob;
-                }
-                catch (BadMultiFactorException) when (attempt < 3)
-                {
-                    // Ignore
-                }
-            }
-        }
-
-        //
-        // Internal
-        //
-
-        internal enum LoginType
-        {
-            DoesntExist,
-            Regular,
-            GoogleAuth_Once,
-            GoogleAuth_Always,
-        }
-
-        internal static LoginType RequestLoginType(string username, RestClient rest)
-        {
-            var parameters = new Dictionary<string, object> {{"login", username}};
-            var response = rest.PostForm<R.LoginType>(LoginTypeEndpoint, parameters);
-            if (response.IsSuccessful)
-            {
-                string type = response.Data.Exists;
-                switch (type)
-                {
-                case "NO":
-                case "NO_UNLIKELY":
-                case "NO_INVALID":
-                    return LoginType.DoesntExist;
-                case "YES":
-                    return LoginType.Regular;
-                case "YES_OTP_NEWDEVICE":
-                    return LoginType.GoogleAuth_Once;
-                case "YES_OTP_LOGIN":
-                    return LoginType.GoogleAuth_Always;
-                }
-
-                throw new UnsupportedFeatureException($"Login type '{type}' is not supported");
-            }
-
-            throw MakeSpecializedError(response);
-        }
-
-        internal static bool IsDeviceRegistered(string username, string deviceId, RestClient rest)
-        {
-            var parameters = new Dictionary<string, object>
-            {
-                {"login", username},
-                {"uki", deviceId},
-            };
-
-            var response = rest.PostForm<R.Status>(VerifyDeviceIdEndpoint, parameters);
-            if (response.IsSuccessful)
-                return response.Data.Code == 200 && response.Data.Message == "OK";
-
-            throw MakeSpecializedError(response);
-        }
-
-        // Always returns a valid passcode. Throws on errors.
-        internal static Ui.Passcode GetPasscodeFromUser(string username,
-                                                        LoginType loginType,
-                                                        int attempt,
-                                                        Ui ui,
-                                                        RestClient rest)
-        {
-            // To login we need the MFA passcode. In case no MFA is set up, the code sent via email.
-            Ui.Passcode passcode = Ui.Passcode.Cancel;
-            switch (loginType)
-            {
-            case LoginType.Regular:
-                // Only trigger the first email right away. On the following attempts wait for the
-                // user to trigger the resend explicitly.
-                var triggerEmail = attempt == 0;
-                do
-                {
-                    if (triggerEmail)
-                        TriggerEmailWithPasscode(username, rest);
-
-                    passcode = ui.ProvideEmailPasscode(attempt);
-                    triggerEmail = true;
-                } while (passcode == Ui.Passcode.Resend);
-                break;
-            case LoginType.GoogleAuth_Once:
-            case LoginType.GoogleAuth_Always:
-                passcode = ui.ProvideGoogleAuthPasscode(attempt);
-                break;
-            default:
-                throw new InternalErrorException("Unknown login type");
-            }
-
-            if (passcode == Ui.Passcode.Cancel)
-                throw new CanceledMultiFactorException("MFA canceled by the user");
-
-            if (passcode.Code.IsNullOrEmpty())
-                throw new InternalErrorException("MFA passcode cannot be null or blank");
-
-            return passcode;
-        }
-
-        internal static void TriggerEmailWithPasscode(string username, RestClient rest)
-        {
-            var parameters = new Dictionary<string, object>
-            {
-                {"login", username},
-                {"isOTPAware", "true"},
-            };
-
-            PerformRegisterDeviceStep(SendTokenEndpoint, parameters, rest);
-        }
-
-        internal static void RegisterDeviceWithToken(string username,
-                                                        string deviceId,
-                                                        string deviceName,
-                                                        string token,
-                                                        RestClient rest)
-        {
-            var parameters = new Dictionary<string, object>
-            {
-                {"devicename", deviceName},
-                {"login", username},
-                {"platform", "server_leeloo"},
-                {"temporary", "0"},
-                {"token", token},
-                {"uki", deviceId},
-            };
-
-            PerformRegisterDeviceStep(RegisterEndpoint, parameters, rest);
-        }
-
-        internal static R.Vault Fetch(string username, string deviceId, IRestTransport transport)
-        {
-            return Fetch_TODO_remove(username, deviceId, new RestClient(transport, BaseApiUrl));
-        }
-
-        internal static R.Vault Fetch_TODO_remove(string username, string deviceId, RestClient rest)
-        {
             var parameters = CommonFetchParameters(username);
             parameters["uki"] = deviceId;
-
-            return Fetch(parameters, rest);
-        }
-
-        internal static R.Vault Fetch(string username, LoginType loginType, string passcode, RestClient rest)
-        {
-            var parameters = CommonFetchParameters(username);
-            switch (loginType)
-            {
-            case LoginType.Regular:
-                parameters["token"] = passcode;
-                break;
-            case LoginType.GoogleAuth_Once:
-            case LoginType.GoogleAuth_Always:
-                parameters["otp"] = passcode;
-                break;
-            default:
-                throw new InternalErrorException($"Unknown login type: {loginType}");
-            }
 
             return Fetch(parameters, rest);
         }
@@ -237,15 +48,6 @@ namespace PasswordManagerAccess.Dashlane
         //
         // Private
         //
-
-        private static void PerformRegisterDeviceStep(string url, Dictionary<string, object> parameters, RestClient rest)
-        {
-            var response = rest.PostForm(url, parameters);
-            if (response.IsSuccessful && response.Content == "SUCCESS")
-                return;
-
-            throw MakeSpecializedError(response);
-        }
 
         private static void CheckForErrors(RestResponse<string> response)
         {
@@ -291,37 +93,11 @@ namespace PasswordManagerAccess.Dashlane
             return token == null || token.Type != JTokenType.String ? defaultValue : (string)token;
         }
 
-        internal static BaseException MakeSpecializedError(RestResponse response)
-        {
-            Uri uri = response.RequestUri;
-
-            if (response.IsHttpError)
-                return new InternalErrorException(
-                    $"HTTP request to {uri} failed with status {response.StatusCode}");
-
-            if (response.IsNetworkError)
-                return new NetworkErrorException(
-                    $"A network error occurred during a request to {uri}", response.Error);
-
-            if (response.Error is JsonException)
-                return new InternalErrorException(
-                    $"Failed to parse JSON response from {uri}", response.Error);
-
-            return new InternalErrorException($"Unexpected response from {uri}", response.Error);
-        }
-
         //
         // Data
         //
 
-        // TODO: Make this configurable
-        private const string DeviceName = "password-manager-access-client";
-
         private const string BaseApiUrl = "https://ws1.dashlane.com/";
-        private const string LoginTypeEndpoint = "7/authentication/exists";
-        private const string VerifyDeviceIdEndpoint = "1/features/getForUser";
         private const string LatestEndpoint = "12/backup/latest";
-        private const string SendTokenEndpoint = "6/authentication/sendtoken";
-        private const string RegisterEndpoint = "6/authentication/registeruki";
     }
 }
--- test/Dashlane/ClientTest.cs
+++ test/Dashlane/ClientTest.cs
@@ -10,43 +10,6 @@ namespace PasswordManagerAccess.Test.Dashlane
     public class ClientTest: TestBase
     {
         //
-        // RequestLoginType
-        //
-
-        [Fact]
-        private void RequestLoginType_makes_post_request_to_specific_endpoint()
-        {
-            var rest = new RestFlow().Post("{'exists': 'YES'}").ExpectUrl(LoginTypeEndpoint);
-            Client.RequestLoginType(Username, rest);
-        }
-
-        [Fact]
-        private void RequestLoginType_requests_with_correct_username()
-        {
-            var rest = new RestFlow().Post("{'exists': 'YES'}").ExpectContent($"login={Username}");
-            Client.RequestLoginType(Username, rest);
-        }
-
-        [Theory]
-        [InlineData("NO", Client.LoginType.DoesntExist)]
-        [InlineData("YES", Client.LoginType.Regular)]
-        [InlineData("YES_OTP_NEWDEVICE", Client.LoginType.GoogleAuth_Once)]
-        [InlineData("YES_OTP_LOGIN", Client.LoginType.GoogleAuth_Always)]
-        internal void RequestLoginType_returns_correct_login_type(string input, Client.LoginType expected)
-        {
-            var rest = new RestFlow().Post($"{{'exists': '{input}'}}");
-            Assert.Equal(expected, Client.RequestLoginType(Username, rest));
-        }
-
-        [Fact]
-        internal void RequestLoginType_throws_on_unknown_login_type()
-        {
-            var rest = new RestFlow().Post("{'exists': 'blah'}");
-            Exceptions.AssertThrowsUnsupportedFeature(() => Client.RequestLoginType(Username, rest),
-                                                      "'blah' is not supported");
-        }
-
-        //
         // Fetch
         //
 
@@ -65,28 +28,6 @@ namespace PasswordManagerAccess.Test.Dashlane
                     .ExpectContent($"login={Username}", $"uki={Uki}")
                     .ExpectContent(s => Assert.DoesNotContain("otp=", s));
             Client.Fetch(Username, Uki, rest);
-        }
-
-        [Fact]
-        private void Fetch_makes_post_request_with_correct_username_email_token_and_no_uki()
-        {
-            var rest = new RestFlow()
-                .Post(GetFixture("empty-vault"))
-                    .ExpectContent($"login={Username}", $"token={Otp}")
-                    .ExpectContent(s => Assert.DoesNotContain("uki=", s));
-            Client.Fetch(Username, Client.LoginType.Regular, Otp, rest);
-        }
-
-        [Theory]
-        [InlineData(Client.LoginType.GoogleAuth_Once)]
-        [InlineData(Client.LoginType.GoogleAuth_Always)]
-        private void Fetch_makes_post_request_with_correct_username_otp_and_no_uki(Client.LoginType loginType)
-        {
-            var rest = new RestFlow()
-                .Post(GetFixture("empty-vault"))
-                    .ExpectContent($"login={Username}", $"otp={Otp}")
-                    .ExpectContent(s => Assert.DoesNotContain("uki=", s));
-            Client.Fetch(Username, loginType, Otp, rest);
         }
 
         [Fact]
@@ -166,94 +107,11 @@ namespace PasswordManagerAccess.Test.Dashlane
         }
 
         //
-        // TriggerEmailWithToken
-        //
-
-        [Fact]
-        private void TriggerEmailWithToken_makes_post_request_to_specific_endpoint()
-        {
-            var rest = new RestFlow().Post("SUCCESS").ExpectUrl(SendEmailEndpoint);
-            Client.TriggerEmailWithPasscode(Username, rest);
-        }
-
-        [Fact]
-        private void TriggerEmailWithToken_makes_post_request_with_correct_username()
-        {
-            var rest = new RestFlow().Post("SUCCESS").ExpectContent($"login={Username}");
-            Client.TriggerEmailWithPasscode(Username, rest);
-        }
-
-        [Fact]
-        private void TriggerEmailWithToken_throws_on_network_error()
-        {
-            var error = new HttpRequestException("Network error");
-            var rest = new RestFlow().Post("", error);
-
-            var e = Exceptions.AssertThrowsNetworkError(() => Client.TriggerEmailWithPasscode(Username, rest),
-                                                        "network error occurred");
-            Assert.Same(error, e.InnerException);
-        }
-
-        [Fact]
-        private void TriggerEmailWithToken_throws_on_invalid_response()
-        {
-            var rest = new RestFlow().Post("NOT A GREAT SUCCESS");
-            Exceptions.AssertThrowsInternalError(() => Client.TriggerEmailWithPasscode(Username, rest));
-        }
-
-        //
-        // RegisterDeviceWithToken
-        //
-
-        [Fact]
-        private void RegisterDeviceWithToken_makes_post_request_to_specific_endpoint()
-        {
-            var rest = new RestFlow().Post("SUCCESS").ExpectUrl(RegisterEndpoint);
-            Client.RegisterDeviceWithToken(Username, Uki, DeviceName, Token, rest);
-        }
-
-        [Fact]
-        private void RegisterDeviceWithToken_makes_post_request_with_correct_username_and_other_parameters()
-        {
-            var rest = new RestFlow()
-                .Post("SUCCESS")
-                .ExpectContent($"login={Username}", $"devicename={DeviceName}", $"uki={Uki}", $"token={Token}");
-            Client.RegisterDeviceWithToken(Username, Uki, DeviceName, Token, rest);
-        }
-
-        [Fact]
-        private void RegisterDeviceWithToken_throws_on_network_error()
-        {
-            var error = new HttpRequestException("Network error");
-            var rest = new RestFlow().Post("", error);
-
-            var e = Exceptions.AssertThrowsNetworkError(
-                () => Client.RegisterDeviceWithToken(Username, Uki, DeviceName, Token, rest),
-                "network error occurred");
-            Assert.Same(error, e.InnerException);
-        }
-
-        [Fact]
-        private void RegisterDeviceWithToken_throws_on_invalid_response()
-        {
-            var rest = new RestFlow().Post("NOT A GREAT SUCCESS");
-            Exceptions.AssertThrowsInternalError(
-                () => Client.RegisterDeviceWithToken(Username, Uki, DeviceName, Token, rest));
-        }
-
-        //
         // Data
         //
 
         private const string Username = "username";
-        private const string Otp = "123456";
-        private const string NoOtp = "";
         private const string Uki = "uki";
-        private const string DeviceName = "device";
-        private const string Token = "token";
-        private const string LoginTypeEndpoint = "/7/authentication/exists";
         private const string FetchEndpoint = "/12/backup/latest";
-        private const string SendEmailEndpoint = "/6/authentication/sendtoken";
-        private const string RegisterEndpoint = "/6/authentication/registeruki";
     }
 }
