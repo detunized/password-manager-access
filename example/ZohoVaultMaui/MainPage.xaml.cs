2c20739fe0b5ab7e61083c7914a4f93129c7232c
Dmitry Yakimenko <detunized@gmail.com>
2024-10-01 15:15:58 +0200

    ZohoVault: call Vault.Open from the MAUI app (ref #137)

--- example/ZohoVaultMaui/MainPage.xaml.cs
+++ example/ZohoVaultMaui/MainPage.xaml.cs
@@ -1,23 +1,61 @@
-﻿namespace ZohoVaultMaui;
+﻿using PasswordManagerAccess.Example.Common;
+using PasswordManagerAccess.ZohoVault;
+using PasswordManagerAccess.ZohoVault.Ui;
 
-public partial class MainPage: ContentPage
+namespace ZohoVaultMaui;
+
+public partial class MainPage : ContentPage
 {
-    int count = 0;
-
     public MainPage()
     {
         InitializeComponent();
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
+    private async void OnCounterClicked(object sender, EventArgs e)
     {
-        count++;
+        try
+        {
+            CounterBtn.Text = "...";
 
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
+            var config = await LoadConfig();
+            var vault = Vault.Open(
+                new Credentials(username: config["username"], password: config["password"], passphrase: config["passphrase"]),
+                new Settings { KeepSession = true },
+                new InvisibleUi(config["google-auth-totp-secret"]),
+                new PrefStorage()
+            );
 
-        SemanticScreenReader.Announce(CounterBtn.Text);
+            CounterBtn.Text = $"Got {vault.Accounts.Length} accounts";
+        }
+        catch (Exception ex)
+        {
+            CounterBtn.Text = ex.Message;
+        }
+    }
+
+    private async Task<Dictionary<string, string>> LoadConfig()
+    {
+        await using var stream = await FileSystem.OpenAppPackageFileAsync("config.yaml").ConfigureAwait(false);
+        using var reader = new StreamReader(stream);
+        var contents = await reader.ReadToEndAsync().ConfigureAwait(false);
+        return Util.ParseConfig(contents);
+    }
+
+    private class InvisibleUi(string totpSecret) : BaseUi, IUi
+    {
+        public Passcode ProvideGoogleAuthPasscode() => new(Util.CalculateGoogleAuthTotp(totpSecret), false);
+    }
+
+    private class PrefStorage : PasswordManagerAccess.Common.ISecureStorage
+    {
+        public string? LoadString(string key) => Preferences.Get(key, null);
+
+        public void StoreString(string key, string? value)
+        {
+            if (value == null)
+                Preferences.Remove(key);
+            else
+                Preferences.Set(key, value);
+        }
     }
 }
